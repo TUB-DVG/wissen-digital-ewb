c81e7d4cf4aa4285998ed59fa079082b5bb290f6
falk cudok <falk.cudok@tu-berlin.de>
2021-12-20 14:04:01 +0100

    simple database setup is working

--- 01_application/webcentral_app/project_listing/admin.py
+++ 01_application/webcentral_app/project_listing/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import Teilprojekt
+from .models import Teilprojekt, Enargus
 
 admin.site.register(Teilprojekt)
+admin.site.register(Enargus)
--- 01_application/webcentral_app/project_listing/models.py
+++ 01_application/webcentral_app/project_listing/models.py
@@ -4,7 +4,7 @@ class Teilprojekt(models.Model):
     fkz = models.CharField(max_length=10, primary_key=True)
     # when  there is a problem try related_name
     enargus_daten = models.ForeignKey('Enargus', null=True, on_delete=models.DO_NOTHING)
-    # return as name, when class is called
+    # return as name, when class is called, eg. tables in admin page
     def __str__(self):
         return self.fkz  # maybe change to the shortname of the project
 
@@ -14,7 +14,13 @@ class Enargus(models.Model):
     laufzeitbeginn = models.DateTimeField(blank=True)
     laufzeitende = models.DateTimeField(blank=True)
     thema = models.CharField(max_length=500, blank=True)
+    verbundbezeichnung = models.CharField(max_length=200, blank=
+                                          True)
     # forschung = models.ForeignKey('Forschung', null=True, on_delete=models.DO_NOTHING)
+    # return as name, when class is called, eg. tables in admin page
+    def __str__(self):
+        return self.verbundbezeichnung  # maybe change to the shortname of the project
+
 
 
 # class Forschung(models.Model):
