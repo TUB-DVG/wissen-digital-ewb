3c221eeaf89ac0534b7c3eacf67fbe31f821b077
falk cudok <falk.cudok@tu-berlin.de>
2022-01-13 12:43:29 +0100

    add local_settings import

--- 01_application/webcentral_app/webcentral_app/settings.py
+++ 01_application/webcentral_app/webcentral_app/settings.py
@@ -134,3 +134,8 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
