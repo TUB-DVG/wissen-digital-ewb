132130e241ab8f0b5a0d9f5fe10a53f64bbeac61
falk cudok <falk.cudok@tu-berlin.de>
2022-01-13 09:03:56 +0100

    make possible to pass strings to the html page

--- 01_application/webcentral_app/project_listing/urls.py
+++ 01_application/webcentral_app/project_listing/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='project_list'),
-    path('<int:fkz>', views.project_view, name='project_view'),
+    path('<str:fkz>', views.project_view, name='project_view'),
     path('search', views.search, name='search'),
 ]
