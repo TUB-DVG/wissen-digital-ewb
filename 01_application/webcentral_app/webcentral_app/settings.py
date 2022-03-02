198292ff87d031bb8d5f9ff206031f1c3c2908ae
tjojo.99 <j.tengler@campus.tu-berlin.de>
2022-03-02 16:47:42 +0100

    Initial commit

--- 01_application/webcentral_app/webcentral_app/settings.py
+++ 01_application/webcentral_app/webcentral_app/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'webcentral',
         'USER': 'postgres',
-        'PASSWORD': 'Anthem2018',
+        'PASSWORD': 'abc123',
         'HOST': 'localhost',
     }
 }
