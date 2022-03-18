b7a09030b67dbb7305a4c19188c54c78e112e123
falk cudok <falk.cudok@tu-berlin.de>
2022-03-18 09:05:23 +0100

    Update settings.py
    put media folder to root level and add media url

--- 01_application/webcentral_app/webcentral_app/settings.py
+++ 01_application/webcentral_app/webcentral_app/settings.py
@@ -131,7 +131,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     Path.joinpath(BASE_DIR, 'webcentral_app/static')
 ]
-MEDIA_ROOT=Path.joinpath(BASE_DIR,'tools_over/Media')
+# Media folder settings
+MEDIA_ROOT=Path.joinpath(BASE_DIR,'media')
+MEDIA_URL = '/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
