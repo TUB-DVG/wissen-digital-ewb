881d3875bc82de0ce5f9df3984c532b569c16882
Firas <drass.firas@yahoo.com>
2022-04-05 13:43:56 +0200

    Added video Interface and Cookie Management system

--- 01_application/webcentral_app/pages/urls.py
+++ 01_application/webcentral_app/pages/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('register/', views.registerPage, name="register"),
 	path('login/', views.loginPage, name="login"),  
 	path('logout/', views.logoutUser, name="logout"),
+    path('Datenschutzhinweis', views.Datenschutzhinweis, name='Datenschutzhinweis'),
 ]
