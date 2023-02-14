68f2d5b6d4bf9391ad73b9da8c885cc1ae26aa71
Tobias <m.degner@tu-berlin.de>
2023-02-14 17:32:32 +0100

    changed the production docker-compose configuration. A nginx-container was added, which is forwarding Requests to a uswgi-application server

--- 01_application/webcentral_app/webcentral_app/settings.py
+++ 01_application/webcentral_app/webcentral_app/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import os
 
 from pathlib import Path
 
@@ -20,13 +21,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l6nvsp#y_3o8--2^h5@903kz%_yx_0=l+i%(2kllzhb=@3+ar('
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get("DEBUG", 0)))
 
 ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS.extend(
+    filter(
+        None,
+        os.environ.get("ALLOWED_HOSTS", '').split(','),
+    )
+)
 
 # Application definition
 
@@ -83,9 +89,9 @@ WSGI_APPLICATION = 'webcentral_app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Empty_database',
-        'USER': 'postgres',
-        'PASSWORD': 'Anthem2018',
+        'NAME': os.environ["POSTGRES_DB"],
+        'USER': os.environ["POSTGRES_USER"],
+        'PASSWORD': os.environ["POSTGRES_PASSWORD"],
         'HOST': 'database',
     }
 }
@@ -128,14 +134,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT= Path.joinpath(BASE_DIR, 'static')
-STATIC_URL = '/static/'
+#STATIC_ROOT= Path.joinpath(BASE_DIR, 'static')
+STATIC_ROOT = "/vol/webcentral/static"
+STATIC_URL = '/static/static/'
 STATICFILES_DIRS = [
     Path.joinpath(BASE_DIR, 'webcentral_app/static')
 ]
 # Media folder settings
-MEDIA_ROOT=Path.joinpath(BASE_DIR,'media')
-MEDIA_URL = '/media/'
+# MEDIA_ROOT=Path.joinpath(BASE_DIR,'media')
+MEDIA_ROOT = "/vol/webcentral/media"
+MEDIA_URL = '/static/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,35 @@
 FROM python:3.10 AS base
 
-RUN useradd --create-home appuser
-# add a env variable user, because some apps read this variable
-ENV USER="appuser"
-USER appuser
+ENV PYTHONUNBUFFERED 1
+
+# RUN useradd --create-home appuser
+# # add a env variable user, because some apps read this variable
+# ENV USER="appuser"
+# USER appuser
 
 WORKDIR /src
 # make sure the ssrc directory is owned by the appuser
-RUN chown appuser:appuser -R /src
+# RUN chown appuser:appuser -R /src
 
 COPY 01_application/requirements.txt .
-RUN pip install -r requirements.txt
+# RUN pip install -r requirements.txt
+
+RUN python -m venv /py && \
+    /py/bin/pip install --upgrade pip && \
+    /py/bin/pip install -r requirements.txt && \
+    adduser --disabled-password --no-create-home appuser && \
+    mkdir -p /vol/webcentral/static && \
+    mkdir -p /vol/webcentral/media && \
+    chown -R appuser:appuser /vol && \
+    chmod -R 755 /vol
+
+# make scripts and venv executables accessible, through path
+ENV PATH="/scripts:/py/bin:$PATH"
 
 # second build stage for production
 
 FROM base AS prod
 USER appuser
 COPY --chown=appuser . /src
+#chmod -R +x /scripts
+CMD ["run.sh"]
