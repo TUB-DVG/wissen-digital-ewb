3ac3625606d174bf2b3f768327069720f9d514af
Tobias <m.degner@tu-berlin.de>
2023-02-21 13:39:18 +0100

    first working exampol

--- 01_application/webcentral_app/webcentral_app/settings.py
+++ 01_application/webcentral_app/webcentral_app/settings.py
@@ -21,10 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+print(os.environ.get("SECRET_KEY"))
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(int(os.environ.get("DEBUG", 0)))
+#DEBUG = bool(int(os.environ.get("DEBUG", 0)))
+
+DEBUG=True
 
 ALLOWED_HOSTS = []
 ALLOWED_HOSTS.extend(
