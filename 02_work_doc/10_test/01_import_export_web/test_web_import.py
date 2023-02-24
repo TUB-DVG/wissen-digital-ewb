3ccde92bcb5c748a8519997a570b7e43852e2568
falk cudok <falk.cudok@tu-berlin.de>
2023-02-24 14:35:19 +0100

    test for import is working

--- 02_work_doc/10_test/01_import_export_web/test_web_import.py
+++ 02_work_doc/10_test/01_import_export_web/test_web_import.py
@@ -5,10 +5,11 @@ from sqlalchemy import create_engine
 
 # read data from the xlsx file
 path_to_xlsx = 'enargus_csv_20220902_test1.xlsx'
-df_xlsx = pd.read_excel(path_to_xlsx)
+df_xlsx = pd.read_excel(path_to_xlsx
+                        ,converters={'PLZ_ZWE': lambda x: f'{x:04}'})
 # print(df_xlxs)
 
-fkz_test = ['03EWR010K', '03EWR002P']
+fkz_test = ['03EWR010K', '03EWR002P', '03EWR008L', '03EWR008I']
 # data from the test xlsx-file
 
 # read data from the data base
@@ -29,6 +30,7 @@ for fkz in fkz_test:
           project_listing_enargus.laufzeitbeginn,
           project_listing_enargus.laufzeitende,
           project_listing_enargus.kurzbeschreibung_de,
+          project_listing_enargus.foerdersumme,
           project_listing_forschung.forschungsprogramm,
           sw_1st.schlagwort_1_id,
           sw_1.schlagwort AS schlagwort_1,
@@ -46,7 +48,9 @@ for fkz in fkz_test:
           bearbeiter.vorname,
           bearbeiter.titel,
           bearbeiter.email,
-          aus_stelle.name AS AusfuehrendeStelle
+          aus_stelle.name AS AusfuehrendeStelle,
+    	  zwe.name as Zuwendungsempfaenger,
+	      zwe_anschrift.plz AS Zwe_plz
    FROM project_listing_teilprojekt
      INNER JOIN project_listing_enargus
        ON project_listing_enargus.enargus_id = project_listing_teilprojekt.enargus_daten_id
@@ -74,27 +78,75 @@ for fkz in fkz_test:
        ON  project_listing_enargus.projektleiter_id = bearbeiter.person_id
      INNER JOIN project_listing_ausfuehrende_stelle AS aus_stelle
        ON  project_listing_enargus.ausfuehrende_stelle_id = aus_stelle.ausfuehrende_stelle_id
+     INNER JOIN project_listing_zuwendungsempfaenger AS zwe
+       ON zwe.zuwendungsempfaenger_id = project_listing_enargus.zuwendsempfanger_id
+    INNER JOIN project_listing_anschrift AS zwe_anschrift
+       ON zwe_anschrift.anschrift_id = zwe.anschrift_id
    WHERE fkz = '%s'
         """ % fkz
     df_db = pd.read_sql_query(query, conn)
     # data from the data base
-    # comparison "Laufzeitbeginn"
+    print(' ')
+    print('Vergleich für Projekt {}'.format(fkz))
+    # comparison "Laufzeitbeginn" (date formate)
     db_laufzeitbeginn = df_db[df_db['fkz'] == fkz]['laufzeitbeginn']
     db_laufzeitbeginn = pd.to_datetime(db_laufzeitbeginn.values[0]).strftime('%Y-%m-%d')
     xlsx_laufzeitbeginn = df_xlsx[df_xlsx['fkz'] == str(fkz)]['Laufzeitbeginn']
     xlsx_laufzeitbeginn = pd.to_datetime(xlsx_laufzeitbeginn.values[0]).strftime('%Y-%m-%d')
+
     print('## Vergleich Laufzeitbeginn bzw. date-feature')
     if str(db_laufzeitbeginn) == str(xlsx_laufzeitbeginn):
-        print('Die Angaben zum Laufzeitbeginn sind gleich.\
-               Datenbank: {} und xlxs: {}'.format(
+        print('Die Angaben zum Laufzeitbeginn sind gleich.')
+        print(' -- Datenbank: {}   \n --      xlxs: {}'.format(
                    db_laufzeitbeginn, xlsx_laufzeitbeginn))
     else:
-        print('Die Angaben zum Laufzeitbeginn sind NICHT gleich.\
-               Datenbank: {} und xlxs: {}'.format(
+        print(' ')
+        print('Die Angaben zum Laufzeitbeginn sind NICHT gleich.')
+        print(' -- Datenbank: {}   \n --      xlxs: {}'.format(
                    db_laufzeitbeginn, xlsx_laufzeitbeginn))
 
-    db_fkz_plz_zwe = df_db[df_db['fkz'] == fkz]['thema']
-    print(db_fkz_plz_zwe)
+    print('## Vergleich Kurzbeschreibung bzw. character formate')
+    # comparison "Kurzbeschreibung" (character formate)
+    db_kurzbeschreibung = df_db[df_db['fkz'] == fkz]['kurzbeschreibung_de']
+    db_kurzbeschreibung = db_kurzbeschreibung.values[0]
+    xlsx_kurzbeschreibung = df_xlsx[df_xlsx['fkz'] == str(fkz)]['Kurzbeschreibung_de']
+    xlsx_kurzbeschreibung = xlsx_kurzbeschreibung.values[0]
+    if db_kurzbeschreibung == xlsx_kurzbeschreibung:
+        print('Die Angaben zum Kurzbeschreibung_de sind gleich.')
+        print(' -- Datenbank: {}   \n --      xlxs: {}'.format(
+                   db_kurzbeschreibung[0:20], xlsx_kurzbeschreibung[0:20]))
+    else:
+        print('Die Angaben zum Kurzbeschreibung_de sind NICHT gleich.')
+        print(' -- Datenbank: {}   \n --      xlxs: {}'.format(
+                   db_kurzbeschreibung[0:20], xlsx_kurzbeschreibung[0:20]))
 
-    xlsx_fkz_plz_zwe= df_xlsx[df_xlsx['fkz']==str(fkz)]['PLZ_ZWE']
-    print(xlsx_fkz_plz_zwe)
+    print('## Vergleich PLZ_zwe bzw. character formate, oft fehlt die erste Null')
+    # comparison "PLZ_zwe" (character formate, often first zero removed)
+    db_PLZ_ZWE = df_db[df_db['fkz'] == fkz]['zwe_plz']
+    db_PLZ_ZWE = str(db_PLZ_ZWE.values[0])
+    xlsx_PLZ_ZWE = df_xlsx[df_xlsx['fkz'] == str(fkz)]['PLZ_ZWE']
+    xlsx_PLZ_ZWE_deb = xlsx_PLZ_ZWE
+    xlsx_PLZ_ZWE = str(xlsx_PLZ_ZWE.values[0])
+    if db_PLZ_ZWE == xlsx_PLZ_ZWE:
+        print('Die Angaben zum PLZ_zwe sind gleich.')
+        print(' -- Datenbank: {}   \n --      xlxs: {}'.format(
+                   db_PLZ_ZWE, xlsx_PLZ_ZWE))
+    else:
+        print('Die Angaben zum PLZ_zwe_de sind NICHT gleich.')
+        print(' -- Datenbank: {}   \n --      xlxs: {}'.format(
+                   db_PLZ_ZWE, xlsx_PLZ_ZWE))
+
+    print('## Vergleich Foerdersumme bzw. decimal formate')
+    # comparison "Foerdersumme" (decimal formate)
+    db_Foerdersumme = df_db[df_db['fkz'] == fkz]['foerdersumme']
+    db_Foerdersumme = str(db_Foerdersumme.values[0])
+    xlsx_Foerdersumme = df_xlsx[df_xlsx['fkz'] == str(fkz)]['Foerdersumme_EUR']
+    xlsx_Foerdersumme = str(xlsx_Foerdersumme.values[0])
+    if db_Foerdersumme == xlsx_Foerdersumme:
+        print('Die Angaben zum Foerdersumme sind gleich.')
+        print(' -- Datenbank: {}   \n --      xlxs: {}'.format(
+                   db_Foerdersumme, xlsx_Foerdersumme))
+    else:
+        print('Die Angaben zum Foerdersumme sind NICHT gleich.')
+        print(' -- Datenbank: {}   \n --      xlxs: {}'.format(
+                   db_Foerdersumme, xlsx_Foerdersumme))
