c85e2b851f03eec264beec651281cf349b9462ad
Tobias <m.degner@tu-berlin.de>
2023-02-27 17:13:13 +0100

    added images, removed unused scripts

--- init.sh
+++
@@ -1,7 +0,0 @@
-docker-compose -f docker-compose.yml -f docker-compose.dev.yml up &
-sleep 5
-docker-compose exec webcentralcontainer python3 01_application/webcentral_app/manage.py makemigrations
-sleep 2
-docker-compose exec webcentralcontainer python3 01_application/webcentral_app/manage.py migrate
-sleep 1
-docker-compose exec webcentralcontainer python3 01_application/webcentral_app/manage.py createsuperuser --no-input
