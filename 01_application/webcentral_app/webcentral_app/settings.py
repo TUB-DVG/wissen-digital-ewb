d282b005a1114ddeef8f7790ebe8d3f83165ae3a
Firas Drass <drass@tu-berlin.de>
2023-03-07 12:31:55 +0000

    Deleted unwanted applications

--- 01_application/webcentral_app/webcentral_app/settings.py
+++ 01_application/webcentral_app/webcentral_app/settings.py
@@ -44,10 +44,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #'django_extensions',
+    'django_extensions',
     'django_plotly_dash.apps.DjangoPlotlyDashConfig',
     'channels',
-    #'channels_redis'
 ]
 
 MIDDLEWARE = [
