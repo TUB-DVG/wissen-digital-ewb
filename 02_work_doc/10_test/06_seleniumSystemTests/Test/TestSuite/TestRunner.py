68cc0e604f631fa8354c6e9ab22b39d772ea2f65
Tobias <m.degner@tu-berlin.de>
2023-05-23 16:15:34 +0200

    started a selenium test in Page-Object-Pattern

---
+++ 02_work_doc/10_test/06_seleniumSystemTests/Test/TestSuite/TestRunner.py
@@ -0,0 +1,28 @@
+import sys
+import os
+import pdb
+sys.path.append(sys.path[0] + "/...")
+# Uncomment if the above example gives you a relative path error
+sys.path.append(os.getcwd())
+ 
+from unittest import TestLoader, TestSuite, TextTestRunner
+from Test.Scripts.testDigitalTools import TestDigitalToolsTab
+#from test.Scripts.test_Google_Search import Google_Search
+ 
+import testtools as testtools
+ 
+if __name__ == "__main__":
+ 
+    test_loader = TestLoader()
+    # Test Suite is used since there are multiple test cases
+    test_suite = TestSuite((
+        test_loader.loadTestsFromTestCase(TestDigitalToolsTab),
+        #test_loader.loadTestsFromTestCase(Google_Search),
+        ))
+ 
+    test_runner = TextTestRunner(verbosity=2)
+    test_runner.run(test_suite)
+ 
+    # Refer https://testtools.readthedocs.io/en/latest/api.html for more information
+    parallel_suite = testtools.ConcurrentStreamTestSuite(lambda: ((case, None) for case in test_suite))
+    parallel_suite.run(testtools.StreamResult())
