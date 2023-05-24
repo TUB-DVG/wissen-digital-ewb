cb30b571739d7cb6a29d70ae39a92f5078e8647d
Tobias <m.degner@tu-berlin.de>
2023-05-24 11:24:59 +0200

    finished multiple system UI tests

--- 02_work_doc/10_test/06_seleniumSystemTests/Src/PageObject/Locators.py
+++ 02_work_doc/10_test/06_seleniumSystemTests/Src/PageObject/Locators.py
@@ -7,6 +7,12 @@ class Locator(object):
     toolListSearchInput = "//input[@id='search-input-tools']"
     toolItemsIdentifer = "//div[@class='col-sm-6 col-lg-4 col-xl-3']"
     searchStrBoxX = "//a[@href='/tool_list/?searched=&k=&l=&lzp=']"
+    searchCategorieSelect = "//select[@id='kategorie-input-tools']"
+    searchLicenceSelect = "//select[@id='lizenz-input-tools']"
+    searchLifecycleSelect = "//select[@id='lzp-input-tools']"
+    searchMagniferButton = "//button[@id='search-submit-tools']"
+    activeSearchFilter = "//li[@class='list-inline-item']"
+    searchResetButton = "//h6[contains(text(), 'Alle')]"
     
     # locators in loginPage
     loginButtonElement = "//input[@class='btn login_btn']"
--- 02_work_doc/10_test/06_seleniumSystemTests/Test/Scripts/testDigitalTools.py
+++ 02_work_doc/10_test/06_seleniumSystemTests/Test/Scripts/testDigitalTools.py
@@ -10,7 +10,7 @@ sys.path.append(sys.path[0] + "/...")
 
 import time
 import os
-import unittest
+import random
 
 from selenium import (
     webdriver,
@@ -51,25 +51,11 @@ class TestDigitalToolsTab(WebDriverSetup):
         titleAfterClickLink = "Überblick über die Anwendungen"
         self.checkPageTitle(titleAfterClickLink)
 
-    def testSearchField(self):
+    def testSearchField(self) -> None:
         """Tests the Search Function in `Digitale Anwendungen`
         
         """
-        self.driver.get("http://127.0.0.1:8070/tool_list/")
-        titleAfterClickLink = "Überblick über die Anwendungen"
-        self.checkPageTitle(titleAfterClickLink)
-
-        loginPageObj = LoginPage(self.driver)
-
-        loginButtonElement = loginPageObj.getLoginButton()
-
-        if loginButtonElement is not None:
-            loginButtonElement.click()
-
-
-        startPage = StartPage(self.driver)   
-        toolListLink = startPage.getToolListLink()
-        toolListLink.click() 
+        self.openToolListAndLogin()
 
         toolListPage = ToolListPage(self.driver)
 
@@ -119,17 +105,185 @@ class TestDigitalToolsTab(WebDriverSetup):
             "After removing search-string 'Ansys', number of tool-items should be 12!",
         )
 
+        searchFieldElement = toolListPage.getSearchInputElement()
+        searchFieldElement.send_keys("B")
 
-        pdb.set_trace()
+        time.sleep(1)
+        numberOfToolItems = len(toolListPage.getListOfToolItems())
+        self.assertLess(
+            numberOfToolItems,
+            12,
+            "After writing 'B' into search-field, the number of Tool-items should be decreased!",
+        )
+        searchFieldElement.send_keys(Keys.RETURN)
+
+        time.sleep(1)
+
+        self.assertEqual(
+            len(toolListPage.getListOfToolItems()),
+            numberOfToolItems,
+            "After pressing 'Return', the number of tool-items should stay the same!",
+        )
+
+        listOfSelectWebElements = []
+        listOfSelectWebElements.append(toolListPage.getSearchCategorySelect())
+        listOfSelectWebElements.append(toolListPage.getSearchLicenceSelect())
+        listOfSelectWebElements.append(toolListPage.getSearchLifecycleSelect())
+
+        numberOfSelectsToBeSet = random.choice([1, 2, 3,])
+
+        chosenElementsList = []
+        for currentNumberOfSelect in range(numberOfSelectsToBeSet):
+            elementInSelectToChoose = random.choice(
+                listOfSelectWebElements[currentNumberOfSelect].options,
+            )
+            if elementInSelectToChoose != listOfSelectWebElements[currentNumberOfSelect].first_selected_option:
+                chosenElementsList.append(elementInSelectToChoose)
+            elementInSelectToChoose.click()
 
 
+        magniferButtonElement = toolListPage.getMagniferButton()
+        magniferButtonElement.click()
 
-
+        listOfActiveSearchFilter = toolListPage.getListOfCurrentlyActiveSearchFilter()
+        
+        for indexInList, chosenElement in enumerate(listOfActiveSearchFilter):
+            if indexInList == 0:
+                self.assertTrue(
+                    "Suchbegriff: B" == chosenElement,
+                    "Text Search Filter is not displayed!",
+                    )
+            else:
+                self.assertTrue(
+                    chosenElement.text == chosenElementsList[indexInList-1].text,
+                    "Active Search-Filter Box does not represent choosen Element from Select-Input!",
+                )
+       
+        resetButtonElement = toolListPage.getResetButton()
+        resetButtonElement.click()
         
 
 
+        self.assertEqual(
+            len(toolListPage.getListOfToolItems()),
+            12,
+            "After pressing 'Return', the number of tool-items should stay the same!",
+        )
 
-    def checkPageTitle(self, pageTitle):
+
+               
+
+        time.sleep(1)
+
+        # listOfSelectWebElements = []
+        # listOfSelectWebElements.append(toolListPage.getSearchCategorieSelect())
+        # listOfSelectWebElements.append(toolListPage.getSearchLicenceSelect())
+        # listOfSelectWebElements.append(toolListPage.getSearchLifecycleSelect())
+
+        numberOfSelectsToBeSet = random.choice([1, 2, 3,])
+
+        chosenElementsList = []
+        for currentNumberOfSelect in range(numberOfSelectsToBeSet):
+            elementInSelectToChoose = random.choice(
+                listOfSelectWebElements[currentNumberOfSelect].options,
+            )
+            if elementInSelectToChoose != listOfSelectWebElements[currentNumberOfSelect].first_selected_option:
+                chosenElementsList.append(elementInSelectToChoose)
+            elementInSelectToChoose.click()
+
+    
+
+    def testSearchFirstCategoryThenString(self):
+        """
+        
+        """
+        self.openToolListAndLogin()
+
+        toolListPage = ToolListPage(self.driver)
+
+        listOfSelectWebElements = [
+            toolListPage.getSearchCategorySelect(),
+            toolListPage.getSearchLicenceSelect(),
+            toolListPage.getSearchLifecycleSelect(),
+        ]
+        numberOfSelectsToBeSet = random.choice([1, 2, 3,])
+
+        chosenElementsList = []
+        for currentNumberOfSelect in range(numberOfSelectsToBeSet):
+            elementInSelectToChoose = random.choice(
+                listOfSelectWebElements[currentNumberOfSelect].options,
+            )
+            if elementInSelectToChoose != listOfSelectWebElements[currentNumberOfSelect].first_selected_option:
+                chosenElementsList.append(elementInSelectToChoose)
+            elementInSelectToChoose.click()
+
+        magniferButtonElement = toolListPage.getMagniferButton()
+        magniferButtonElement.click()
+        listOfActiveSearchFilter = toolListPage.getListOfCurrentlyActiveSearchFilter()
+        pdb.set_trace()
+        for indexInList, chosenElement in enumerate(listOfActiveSearchFilter):
+            self.assertTrue(
+                chosenElement.text == chosenElementsList[indexInList].text,
+                "Active Search-Filter Box does not represent choosen Element from Select-Input!",
+            )      
+
+        searchFieldElement = toolListPage.getSearchInputElement()
+        searchFieldElement.send_keys("Hi")
+
+        toolListPage.getMagniferButton().click()    
+        listOfActiveSearchFilter = toolListPage.getListOfCurrentlyActiveSearchFilter()
+        chosenElementsList.insert(0, "Suchbegriff: Hi")
+
+        for indexInList, chosenElement in enumerate(listOfActiveSearchFilter):
+            if indexInList == 0:
+                self.assertTrue(
+                    chosenElementsList[0] == chosenElement,
+                    "Text Search Filter is not displayed!",
+                    )
+            else:
+                self.assertTrue(
+                    chosenElement.text == chosenElementsList[indexInList].text,
+                    "Active Search-Filter Box does not represent choosen Element from Select-Input!",
+                )
+
+    def testIsRedirectedToPreviousPageAfterLogin(self) -> None:
+        """Tests, if the user gets redirected to the previous page after login.
+        
+        """
+        self.driver.get("http://127.0.0.1:8070/tool_list/")
+        time.sleep(1)
+
+        if self.driver.title == "Login":
+            loginPageObj = LoginPage(self.driver)
+            loginPageObj.getLoginButton().click()
+        
+        self.assertTrue(
+            self.driver.title == "Überblick über die Anwendungen",
+            "Login Does not redirect back to tool_list!",
+        )
+
+
+    def openToolListAndLogin(self) -> None:
+        """Helper-method, which connects to tool-list page.
+        
+        """
+        self.driver.get("http://127.0.0.1:8070/tool_list/")
+        titleAfterClickLink = "Überblick über die Anwendungen"
+        self.checkPageTitle(titleAfterClickLink)
+
+        loginPageObj = LoginPage(self.driver)
+
+        loginButtonElement = loginPageObj.getLoginButton()
+
+        if loginButtonElement is not None:
+            loginButtonElement.click()
+
+
+        startPage = StartPage(self.driver)   
+        toolListLink = startPage.getToolListLink()
+        toolListLink.click() 
+
+    def checkPageTitle(self, pageTitle) -> None:
         """
         
         """
