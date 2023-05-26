0c91bcb9d7f2c5d90c13d9a7dd112fa25a2e8245
falk cudok <falk.cudok@tu-berlin.de>
2023-05-26 15:00:19 +0200

    first version of result of the StartSearch
    no logic is implemented

--- 01_application/webcentral_app/StartSearch/urls.py
+++ 01_application/webcentral_app/StartSearch/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.startSearch, name='StartSearch'),
+    path('ResultSearch', views.resultSearch, name='ResultSearch'),
 ]
--- 01_application/webcentral_app/StartSearch/views.py
+++ 01_application/webcentral_app/StartSearch/views.py
@@ -5,3 +5,8 @@ from django.shortcuts import render
 def startSearch(request):
     """View function of the start page including central search function."""
     return render(request, 'StartSearch/StartSearch.html')
+
+
+def resultSearch(request):
+    """View function of the result page of the central search function."""
+    return render(request, 'StartSearch/ResultSearch.html')
