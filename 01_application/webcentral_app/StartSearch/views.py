f21183995f2d2c74cadc9feabefc9ce7c23ab7b0
falk cudok <falk.cudok@tu-berlin.de>
2023-06-01 17:02:11 +0200

    add querySet for projects

--- 01_application/webcentral_app/StartSearch/views.py
+++ 01_application/webcentral_app/StartSearch/views.py
@@ -1,6 +1,7 @@
 """View functions for start page and start page search."""
 from django.shortcuts import render
 from tools_over.models import Tools
+from project_listing.models import Teilprojekt
 from django.db.models import Q
 
 
@@ -15,13 +16,31 @@ def resultSearch(request):
     searchInput = request.POST.get("searchValue", None)
     # read data from data base
     # filtered tools
-    criterionOne = Q(bezeichnung__icontains=searchInput)
-    criterionTwo = Q(kurzbeschreibung__icontains=searchInput)
-    filteredTools = Tools.objects.filter(
-        criterionOne | criterionTwo)
+    criterionToolsOne = Q(bezeichnung__icontains=searchInput)
+    criterionToolsTwo = Q(kurzbeschreibung__icontains=searchInput)
+    filteredTools = Tools.objects.values("bezeichnung",
+                                         "kurzbeschreibung"
+                                         ).filter(criterionToolsOne |
+                                                  criterionToolsTwo)
+    # filtered projects
+    criterionProjectsOne = Q(
+        enargus_daten__verbundbezeichnung__icontains=searchInput)
+    criterionProejctsTwo = Q(
+        enargus_daten__kurzbeschreibung_de__icontains=searchInput)
+    filteredProjects = Teilprojekt.objects.values("fkz",
+                                                  "enargus_daten__verbundbezeichnung",
+                                                  "enargus_daten__kurzbeschreibung_de"
+                                                  ).filter(
+                                                      criterionProjectsOne |
+                                                      criterionProejctsTwo)
+    # concatenate the filtered data sets to one data set,
+    # which can used as input for the table in html
+
     # debuging section, delete when not needed anymore
     print(searchInput)
     print(filteredTools)
+    print(filteredProjects)
+    print("Anzahl der gefilterten Projekte: %s" % filteredProjects.count())
 
     context = {
         "searchValue": searchInput,
