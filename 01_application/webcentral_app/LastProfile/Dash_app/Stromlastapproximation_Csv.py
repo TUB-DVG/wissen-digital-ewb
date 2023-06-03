ea793db380f3a63a170718ca64a612f5800fd573
Firas <drass.firas@yahoo.com>
2023-06-03 19:49:48 +0200

    updated files to follow style guide

--- 01_application/webcentral_app/LastProfile/Dash_app/Stromlastapproximation_Csv.py
+++ 01_application/webcentral_app/LastProfile/Dash_app/Stromlastapproximation_Csv.py
@@ -2,35 +2,30 @@
 
 import pathlib
 import os.path 
-
+import pandas as pd
+import math
 
 PATH = pathlib.Path(__file__).parent.resolve() 
 DATA_PATH = os.path.join(PATH , 'Strom_Approximation.csv') 
 
-def Stromapproximation(application:int,power_requirement:int):
-    import pandas as pd
-    import math
+def currentApproximation(application:int,powerRequirement:int):
+
     df = pd.read_csv(DATA_PATH)
     try:    
-        Summe=float(df['Summe'][application+1])
+        Summe = float(df['Summe'][application+1])
     except:
-        Summe =0
-
-    #print(Summe)
-    #print(df['Strom_Standardlastprofil']['Summe'])
-    lastgang=[]
-    #print(df.iloc[:,Anwendung])
+        Summe = 0
+    lastgang = []
     for i in range(3,8763):
         try:
-            lastgang.append(float(df.iloc[:,application][i]) *(power_requirement/Summe))
+            lastgang.append(float(df.iloc[:,application][i])*(powerRequirement/Summe))
             
         except:
             lastgang.append(0)
-    sum=math.fsum(lastgang)
-    lastgang2=[]
+    sum = math.fsum(lastgang)
+    lastgang2 = []
     for i in range (0, len(lastgang)):
-        lastgang2.append( lastgang[i]*(power_requirement/sum))
-
+        lastgang2.append( lastgang[i]*(powerRequirement/sum))
     return lastgang2
 
 
--- 01_application/webcentral_app/LastProfile/Dash_app/Warmelastapproximation_csv.py
+++ 01_application/webcentral_app/LastProfile/Dash_app/Warmelastapproximation_csv.py
@@ -9,231 +9,241 @@ from typing import Tuple
 PATH = pathlib.Path(__file__).parent.resolve() 
 DATA_PATH = os.path.join(PATH , 'Wärme_Strom.csv') 
 TRY_PATH = os.path.join(PATH , 'TRY2015_524124130664_Jahr.csv') 
-def warmelast(Application:int,heat_demand:int,Station:int,start_date:str,end_date:str,referenceyear:str)-> Tuple[int, pd.DataFrame,pd.DataFrame]:
-    if referenceyear=="on":
+def heatLoad(application:int,heatDemand:int,station:int,startDate:str,endDate:str,
+             referenceYear:str)-> Tuple[int, pd.DataFrame,pd.DataFrame]:
+    if referenceYear == "on":
         #Setting up the resolution for data filtering
-        Resolution='HOURLY'
+        Resolution = 'HOURLY'
      
         # Parameter variable selection
-        Parameter='TEMPERATURE_AIR_MEAN_200'
+        Parameter = 'TEMPERATURE_AIR_MEAN_200'
         #Setting up the Period
-        Period='RECENT'
+        Period = 'RECENT'
         # Acquiring all the stations that provide data according to selected filters
         stations = DwdObservationRequest(
-            parameter=Parameter,
-            resolution=Resolution,
-            period=Period
+            parameter = Parameter,
+            resolution = Resolution,
+            period = Period
             )
-        data= stations.filter_by_station_id(station_id=Station)
-        station_data = data.values.all().df
+        data = stations.filter_by_station_id(station_id = station)
+        stationData = data.values.all().df
 
         #Input from the server is Im Kelvin converted to celsius
-        station_data['value']=station_data['value'].apply(lambda x: x - 273.15)
+        stationData['value'] = stationData['value'].apply(lambda x: x - 273.15)
     else:
-        station_data=pd.read_csv(TRY_PATH)
-        station_data["date"] = pd.to_datetime(station_data[["YEAR","MONTH", "DAY", "HOUR"]], format="%Y-%m-%d %H",utc=True)
+        stationData = pd.read_csv(TRY_PATH)
+        stationData["date"] = pd.to_datetime(stationData[["YEAR","MONTH", 
+                                                            "DAY", "HOUR"]],
+                                                            format = "%Y-%m-%d %H",
+                                                            utc = True)
 
     # Fill in the missing entries in the wetterdient data and mark them
-    Fehlende_werte=0
-    station_data['fehlend'] = 'False'
-    for i in range (0,len(station_data.index)):
-        if math.isnan(station_data['value'][i]):
-            Fehlende_werte+=1
-            station_data['value'][i]=station_data['value'][i-24]
-            station_data['fehlend'][i]='True'
+    missingValues=0
+    stationData['fehlend'] = 'False'
+    for i in range (0,len(stationData.index)):
+        if math.isnan(stationData['value'][i]):
+            missingValues += 1
+            stationData['value'][i] = stationData['value'][i-24]
+            stationData['fehlend'][i] = 'True'
 
     # Prepare csv file to read factors
-    df_warme = pd.read_csv(DATA_PATH)
+    dfHeat = pd.read_csv(DATA_PATH)
 
 
     #Load regression coefficients
-    A=float(df_warme.iloc[:,9][Application+4])
-    B=float(df_warme.iloc[:,10][Application+4])
-    C=float(df_warme.iloc[:,11][Application+4])
-    D=float(df_warme.iloc[:,12][Application+4])
+    A = float(dfHeat.iloc[:,9][application+4])
+    B = float(dfHeat.iloc[:,10][application+4])
+    C = float(dfHeat.iloc[:,11][application+4])
+    D = float(dfHeat.iloc[:,12][application+4])
 
     #Load weekday factors
-    W_Tag=[]
+    weekDay = []
     ##Montag
-    W_Tag.append(float(df_warme.iloc[:,9][Application+23]))
+    weekDay.append(float(dfHeat.iloc[:,9][application+23]))
     ##Dienstag
-    W_Tag.append(float(df_warme.iloc[:,10][Application+23]))
+    weekDay.append(float(dfHeat.iloc[:,10][application+23]))
     ## Mittwoch
-    W_Tag.append(float(df_warme.iloc[:,11][Application+23]))
+    weekDay.append(float(dfHeat.iloc[:,11][application+23]))
     ##Donnerstag
-    W_Tag.append(float(df_warme.iloc[:,12][Application+23]))
+    weekDay.append(float(dfHeat.iloc[:,12][application+23]))
     ##Freitag
-    W_Tag.append(float(df_warme.iloc[:,13][Application+23]))
+    weekDay.append(float(dfHeat.iloc[:,13][application+23]))
     ##Samstag
-    W_Tag.append(float(df_warme.iloc[:,14][Application+23]))
+    weekDay.append(float(dfHeat.iloc[:,14][application+23]))
     ##Sonntag
-    W_Tag.append(float(df_warme.iloc[:,15][Application+23]))
-    #print(W_Tag)
+    weekDay.append(float(dfHeat.iloc[:,15][application+23]))
+    #print(weekDay)
 
     #Calculation of h
-    h=[]
-    for i in range(0,station_data.shape[0]):
-        T_average=0
+    h = []
+    for i in range(0,stationData.shape[0]):
+        tAverage = 0
         LaufV = i 
-        j=0
+        j = 0
    
-        while j<24 and LaufV <station_data.shape[0] :
-            T_moment=float(station_data['value'][LaufV])
-            T_average=T_average+T_moment/24
-            LaufV=LaufV+1
-            j+=1
+        while j<24 and LaufV <stationData.shape[0] :
+            tMoment = float(stationData['value'][LaufV])
+            tAverage = tAverage+tMoment/24
+            LaufV = LaufV + 1
+            j += 1
 
         
-        h.append(round( A / (1 + (B / (T_average - 40)) ** C) + D,14))
+        h.append(round( A / (1 + (B / (tAverage - 40)) ** C) + D,14))
 
     #Load hour factors
-    Zeile_Anfang=Application*13 -24
+    lineStart=application*13 -24
 
     #Load hour factors for Mondays
-    Column=18
+    column=18
     F_Montag = [[0 for x in range(24)] for y in range(10)] 
     for i in range(0,24):
         for j in range(0,10):
-            F_Montag[j][i]=df_warme.iloc[:,Column+i][Zeile_Anfang+j]
+            F_Montag[j][i]=dfHeat.iloc[:,column+i][lineStart+j]
 
 
     #Load hour factors for Tuesdays
-    Column=44
-    F_Dienstag = [[0 for x in range(24)] for y in range(10)] 
+    column=44
+    factorTuesday = [[0 for x in range(24)] for y in range(10)] 
     for i in range(0,24):
         for j in range(0,10):
-            F_Dienstag[j][i]=df_warme.iloc[:,Column+i][Zeile_Anfang+j]
+            factorTuesday[j][i]=dfHeat.iloc[:,column+i][lineStart+j]
 
     #Load hour factors for Wednesdays
 
-    Column=70
-    F_Mittwoch = [[0 for x in range(24)] for y in range(10)] 
+    column=70
+    factorWednesday = [[0 for x in range(24)] for y in range(10)] 
     for i in range(0,24):
         for j in range(0,10):
-            F_Mittwoch[j][i]=df_warme.iloc[:,Column+i][Zeile_Anfang+j]
+            factorWednesday[j][i] = dfHeat.iloc[:,column+i][lineStart+j]
 
     #Load hour factors for Thursdays
-    Column=96
-    F_Donnerstag = [[0 for x in range(24)] for y in range(10)] 
+    column=96
+    factorThursday = [[0 for x in range(24)] for y in range(10)] 
     for i in range(0,24):
         for j in range(0,10):
-            F_Donnerstag[j][i]=df_warme.iloc[:,Column+i][Zeile_Anfang+j]
+            factorThursday[j][i] = dfHeat.iloc[:,column+i][lineStart+j]
 
     #Load hour factors for Fridays
-    Column=122
-    F_Freitag= [[0 for x in range(24)] for y in range(10)] 
+    column=122
+    factorFriday= [[0 for x in range(24)] for y in range(10)] 
     for i in range(0,24):
         for j in range(0,10):
-            F_Freitag[j][i]=df_warme.iloc[:,Column+i][Zeile_Anfang+j]
+            factorFriday[j][i] = dfHeat.iloc[:,column+i][lineStart+j]
 
 
     #Load hour factors for Saturdays
-    Column=148
-    F_Samstag= [[0 for x in range(24)] for y in range(10)] 
+    column=148
+    factorSaturday= [[0 for x in range(24)] for y in range(10)] 
     for i in range(0,24):
         for j in range(0,10):
-            F_Samstag[j][i]=df_warme.iloc[:,Column+i][Zeile_Anfang+j]
+            factorSaturday[j][i]=dfHeat.iloc[:,column+i][lineStart+j]
 
     #Load hour factors for Sundays
-    Column=174
-    F_Sonntag= [[0 for x in range(24)] for y in range(10)] 
+    column=174
+    factorSunday= [[0 for x in range(24)] for y in range(10)] 
     for i in range(0,24):
         for j in range(0,10):
-            F_Sonntag[j][i]=df_warme.iloc[:,Column+i][Zeile_Anfang+j]
+            factorSunday[j][i] = dfHeat.iloc[:,column+i][lineStart+j]
 
     # Calculation of the hourly heat demand
-    Q_average=heat_demand
-    Q=[]
+    Q_average = heatDemand
+    Q = []
     #LaufV = 0
     #k = 0
 
     # Calculate mean h
     h_average = 0
-    for i in range(0,station_data.shape[0]):
-        h_average=h_average+h[i]
+    for i in range(0,stationData.shape[0]):
+        h_average = h_average+h[i]
 
-    h_average = h_average / (station_data.shape[0])
+    h_average = h_average / (stationData.shape[0])
 
     i=0
-    for k in range (0,station_data.shape[0]):
+    for k in range (0,stationData.shape[0]):
                 # Reset hours after 24 hours
                 if i == 24 :
-                    i=0
+                    i = 0
           
                 #Selection of the line depending on the outside temperature
-                if station_data['value'][k] <=-15:
-                    Zeile=0
-                elif station_data['value'][k] <=-10 and station_data['value'][k] >-15:
-                    Zeile=1
-                elif station_data['value'][k] <=-5 and station_data['value'][k] >-10:
-                    Zeile=2
-                elif station_data['value'][k] <=0 and station_data['value'][k] >-5:
-                    Zeile=3
-                elif station_data['value'][k] <=5 and station_data['value'][k] >0:
-                    Zeile=4
-                elif station_data['value'][k] <=10 and station_data['value'][k] >5:
-                    Zeile=5
-                elif station_data['value'][k] <=15 and station_data['value'][k] >10:
-                    Zeile=6
-                elif station_data['value'][k] <=20 and station_data['value'][k] >15:
-                    Zeile=7
+                if stationData['value'][k] <=-15:
+                    line = 0
+                elif stationData['value'][k] <=-10 and stationData['value'][k] >-15:
+                    line = 1
+                elif stationData['value'][k] <=-5 and stationData['value'][k] >-10:
+                    line = 2
+                elif stationData['value'][k] <=0 and stationData['value'][k] >-5:
+                    line = 3
+                elif stationData['value'][k] <=5 and stationData['value'][k] >0:
+                    line = 4
+                elif stationData['value'][k] <=10 and stationData['value'][k] >5:
+                    line = 5
+                elif stationData['value'][k] <=15 and stationData['value'][k] >10:
+                    line = 6
+                elif stationData['value'][k] <=20 and stationData['value'][k] >15:
+                    line = 7
                     #Here is a difference
-                elif station_data['value'][k] <=25 and station_data['value'][k] >20:
-                    Zeile=8
+                elif stationData['value'][k] <=25 and stationData['value'][k] >20:
+                    line = 8
                 else:
-                    Zeile=9
+                    line = 9
 
                 #Tagessumme h
-                h_sum=0
-                LaufV= k 
-                j=0
-                while j<24 and LaufV<station_data.shape[0]:
-                    h_sum=h_sum + h[LaufV]
-                    LaufV=LaufV+1
-                    j+=1
+                h_sum = 0
+                LaufV = k 
+                j = 0
+                while j<24 and LaufV<stationData.shape[0]:
+                    h_sum = h_sum + h[LaufV]
+                    LaufV = LaufV+1
+                    j += 1
                 #Selecting the right hour factor
-                if station_data['date'][k].weekday() ==0 :
-                    F=F_Montag[Zeile] [i]
-                elif station_data['date'][k].weekday() == 1 :
-                        F = F_Dienstag[Zeile] [i]
-                elif station_data['date'][k].weekday() == 2 :
-                        F = F_Mittwoch[Zeile] [i]
-                elif station_data['date'][k].weekday() == 3:
-                        F = F_Donnerstag[Zeile] [i]
-                elif station_data['date'][k].weekday() == 4:
-                        F = F_Freitag[Zeile] [i]
-                elif station_data['date'][k].weekday() == 5 :
-                        F = F_Samstag[Zeile] [i]
+                if stationData['date'][k].weekday() == 0 :
+                    F=F_Montag[line] [i]
+                elif stationData['date'][k].weekday() == 1 :
+                        F = factorTuesday[line] [i]
+                elif stationData['date'][k].weekday() == 2 :
+                        F = factorWednesday[line] [i]
+                elif stationData['date'][k].weekday() == 3:
+                        F = factorThursday[line] [i]
+                elif stationData['date'][k].weekday() == 4:
+                        F = factorFriday[line] [i]
+                elif stationData['date'][k].weekday() == 5 :
+                        F = factorSaturday[line] [i]
                 else:
-                    F = F_Sonntag[Zeile] [i]
+                    F = factorSunday[line] [i]
                 try:
                    
-                    Q.append( h_sum * (Q_average / h_average) * F * W_Tag[station_data['date'][k].weekday()])
+                    Q.append( h_sum * (Q_average / h_average) * F * 
+                             weekDay[stationData['date'][k].weekday()])
                 except ZeroDivisionError:
                     z = 0
               
                 #Increment hours
-                i=i+1
+                i = i+1
                 
     #Calculation of the approximate annual heat requirement
-    Q_sum=0
-    for i in range (0,station_data.shape[0]):
-        Q_sum=Q_sum+Q[i]
+    Q_sum = 0
+    for i in range (0,stationData.shape[0]):
+        Q_sum = Q_sum+Q[i]
         
 
     # Correction of the heat requirement, calculation of the WW_share
-    Q_WW=[]
-    Q_input=heat_demand
-    for i in range(0,station_data.shape[0]):
+    Q_WW = []
+    Q_input = heatDemand
+    for i in range(0,stationData.shape[0]):
     
-        Q[i]=Q[i]*(Q_input/Q_sum)
+        Q[i] = Q[i]*(Q_input/Q_sum)
      
         Q_WW.append(D*(Q[i]/h[i]))
 
-    start=station_data.index[station_data.date == pd.Timestamp(start_date+" 01:00:00+00:00")].tolist()[0]
-    end=station_data.index[station_data.date == pd.Timestamp(end_date+" 23:00:00+00:00")].tolist()[0] +1
+    start=stationData.index[stationData.date == 
+                            pd.Timestamp(startDate+" 01:00:00+00:00")].tolist()[0]
+    end=stationData.index[stationData.date == 
+                          pd.Timestamp(endDate+" 23:00:00+00:00")].tolist()[0] +1
 
-    heat_approximation_df = pd.DataFrame({'Time':station_data['date'][start:end],'Last':Q[start:end],'WW_Last':Q_WW[start:end],'fehlend':station_data['fehlend'][start:end]})
+    heatApproximationDf = pd.DataFrame({'Time':stationData['date'][start:end],
+                                        'Last':Q[start:end],
+                                        'WW_Last':Q_WW[start:end],
+                                        'fehlend':stationData['fehlend'][start:end]})
 
-    return Fehlende_werte,heat_approximation_df
+    return missingValues,heatApproximationDf
 
