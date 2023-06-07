9bda832aa18b8259aab3379d8416d334d89960b8
falk cudok <falk.cudok@tu-berlin.de>
2023-06-07 17:01:13 +0200

    add doc strings

--- 01_application/webcentral_app/StartSearch/tables.py
+++ 01_application/webcentral_app/StartSearch/tables.py
@@ -4,9 +4,13 @@ import django_tables2 as tables
 
 
 class ResultTable(tables.Table):
+    """Class defines the table of the search results."""
+
     selection = tables.CheckBoxColumn(accessor="pk", orderable=False)
     name = tables.Column()
     surname = tables.Column()
 
     class Meta:
+        """Meta data of the table for the search results."""
+
         template_name = "django_tables2/bootstrap.html"
