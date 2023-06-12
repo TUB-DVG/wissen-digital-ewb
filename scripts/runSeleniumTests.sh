95e2c5f94064bf3ecedf36425de7ce178f5bd877
Tobias <m.degner@tu-berlin.de>
2023-06-12 15:29:42 +0200

    rm clear from runSeleniumTest

--- scripts/runSeleniumTests.sh
+++ scripts/runSeleniumTests.sh
@@ -6,7 +6,7 @@ set +a
 docker-compose -f docker-compose.yml -f docker-compose.prod.yml up --force-recreate --detach
 
 bash postgres/restoreDB.sh
-clear
+
 cd 02_work_doc/10_test/06_seleniumSystemTests/
 . ../testingVenv/bin/activate
 python Test/TestSuite/TestRunner.py
