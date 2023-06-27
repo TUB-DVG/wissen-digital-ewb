8bec254307c842c1049806e11b124fc21b78de82
Firas <drass.firas@yahoo.com>
2023-06-27 13:21:47 +0200

    Deleted Review feature

--- 01_application/webcentral_app/Datasets/urls.py
+++ 01_application/webcentral_app/Datasets/urls.py
@@ -5,6 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='dataset_list'),
     path('<str:id>', views.dataset_view, name='dataset_view'),
-    #path('Post_Review/<str:id>', views.Post_Review, name='Post_Review'),
 
 ]
