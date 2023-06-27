ce390b5eec8c315a80611a35ed49242a2084e6c8
Firas <drass.firas@yahoo.com>
2023-06-27 13:21:47 +0200

    Deleted Review feature

--- 01_application/webcentral_app/tools_over/urls.py
+++ 01_application/webcentral_app/tools_over/urls.py
@@ -5,6 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='tool_list'),
     path('<str:id>', views.toolView, name='tool_view'),
-    path('Post_Review/<str:id>', views.postReview, name='post_review'),
 
 ]
