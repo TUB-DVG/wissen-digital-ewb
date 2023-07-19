97c1c4c803e25c74f808c24d2381dbb549b0fd50
falk cudok <falk.cudok@tu-berlin.de>
2023-07-19 16:08:38 +0200

    activate new start page StartSearch

--- 01_application/webcentral_app/StartSearch/urls.py
+++ 01_application/webcentral_app/StartSearch/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 
 from . import views
 
-app_name = "StartSearch"  # includes namespacing URL names
 urlpatterns = [
     path("", views.startSearch, name="StartSearch"),
     path("ResultSearch", views.resultSearch, name="ResultSearch"),
