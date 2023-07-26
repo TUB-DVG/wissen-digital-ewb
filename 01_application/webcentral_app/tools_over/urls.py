550ad36ab8dc658c13067f7756f780e1301e74ff
Tobias <m.degner@tu-berlin.de>
2023-07-26 10:53:23 +0200

    templates for buisnessApplications

--- 01_application/webcentral_app/tools_over/urls.py
+++ 01_application/webcentral_app/tools_over/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='tool_list'),
     path('<str:id>', views.toolView, name='tool_view'),
-
+    # path('', views.indexBuisnessApplication, name='buisnessModelApplication')
 ]
--- 01_application/webcentral_app/tools_over/views.py
+++ 01_application/webcentral_app/tools_over/views.py
@@ -23,7 +23,10 @@ class UpdateProperties:
 
 def index(request):
     """Shows the list of all projects including some key features."""
-    tools = Tools.objects.filter(classification__classification="Werkzeug", focus__focus="Technisch") # reads all data from table Teilprojekt
+    tools = Tools.objects.filter(
+        classification__classification="Werkzeug", 
+        focus__focus="Technisch",
+    ) # reads all data from table Teilprojekt
     filteredBy = [None]*3
     searched=None
  
@@ -38,6 +41,8 @@ def index(request):
             lifeCyclePhase__icontains=lifeCyclePhase,
             licence__icontains=licence,
             name__icontains=searched,
+            focus__focus="Technisch",
+            classification__classification="Werkzeug",
         )
         filteredBy = [usage, licence, lifeCyclePhase]
               
@@ -75,11 +80,12 @@ def index(request):
 
     return render(request, 'tools_over/tool-listings.html', context)
 
-def indexApplication(request):
+def indexBuisnessApplication(request):
     """serves a request for digital applications search
     
     """
     applications = Tools.objects.filter(classification__classification="Digitale Anwendung", focus__focus="Betrieblich") # reads all data from table Teilprojekt
+    
     filteredBy = [None]*3
     searched=None
  
@@ -89,15 +95,17 @@ def indexApplication(request):
         licence = request.GET.get('l')
         lifeCyclePhase = request.GET.get('lcp')
         searched = request.GET.get('searched')
-        tools = Tools.objects.filter(
+        applications = Tools.objects.filter(
             usage__icontains=usage,
             lifeCyclePhase__icontains=lifeCyclePhase,
             licence__icontains=licence,
             name__icontains=searched,
+            classification__classification="Digitale Anwendung",
+            focus__focus="Betrieblich",
         )
         filteredBy = [usage, licence, lifeCyclePhase]
               
-    applications = list(sorted(tools, key=lambda obj:obj.name))
+    applications = list(sorted(applications, key=lambda obj:obj.name))
     toolsPaginator= Paginator(applications,12)
     pageNum= request.GET.get('page',None)
     page=toolsPaginator.get_page(pageNum)
@@ -129,7 +137,36 @@ def indexApplication(request):
         'lifeCyclePhase': filteredBy[2]
     }
 
-    return render(request, 'tools_over/tool-listings.html', context)  
+    return render(request, 'tools_over/buisnessApplications-listings.html', context)  
+
+def buisnessApplicationView(request, id):
+    """Shows of the key features one project"""
+    tool = get_object_or_404(Tools, pk= id)
+    usages = tool.usage.split(", ")
+    lifeCyclePhases = tool.lifeCyclePhase.split(", ")
+    continuousUpdates = tool.lastUpdate
+    
+    lastUpdate = UpdateProperties('bi bi-patch-exclamation-fill', 'letztes Update', 'text-danger')
+    continuousUpdates = UpdateProperties('fas fa-sync', 'Updates', 'text-success')
+
+    #changing labels and icon
+    updateProperties = lastUpdate
+    if (tool.lastUpdate == 'laufend'): # continuous
+        updateProperties = continuousUpdates
+
+
+    context = {
+        'tool': tool,
+        'usages': usages,
+        'lifeCyclePhases': lifeCyclePhases,
+        'lastUpdate': updateProperties,
+        'lastUpdateClass': updateProperties.className,
+        'lastUpdateColor': updateProperties.colorClass,
+        'lastUpdateLabel': updateProperties.label,
+       
+    }
+
+    return render(request, 'tools_over/buisnessApplications-detail.html', context)
 
 
 def toolView(request, id):
