7fe748e87a4cb0935e80ba103d8461bd1f5a2d4a
Tobias <m.degner@tu-berlin.de>
2023-08-15 16:49:08 +0200

    tested if BIM2SIM in results when search for Bim on MainPage

--- 02_work_doc/10_test/06_seleniumSystemTests/Src/PageObject/Pages/startPage.py
+++ 02_work_doc/10_test/06_seleniumSystemTests/Src/PageObject/Pages/startPage.py
@@ -33,6 +33,35 @@ class StartPage(object):
             By.XPATH,
             Locator.linkToImpressum, 
         )
-
     
+    def getSearchInputField(self):
+        """Return Search-Input Webelement from MainPage
+        
+        """
+        return self.driver.find_element(
+            By.XPATH,
+            Locator.inputSearchField, 
+        )
+    
+    def getSearchResults(self):
+        """Return the Search-Results
 
+        The Search-Results are stored in a Table. One Row is represented 
+        by an <tr></tr> Element. Therefore all <tr>-webelements are returned.
+        This includes also the header-row.
+        
+        """
+        return self.driver.find_elements(
+            By.XPATH, 
+            Locator.resultElements,
+        )
+
+    def getFirstColumn(self, columnWebelement):
+        """Return first Column to a row-html <tr>-webelement
+        
+        """
+
+        return columnWebelement.find_element(
+            By.XPATH, 
+            Locator.firstColumnToRow,
+        )
