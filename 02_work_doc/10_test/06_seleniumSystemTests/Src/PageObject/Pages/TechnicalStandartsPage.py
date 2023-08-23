b0b967b544f933b0629a2ddcaf67df69b570c870
Tobias <m.degner@tu-berlin.de>
2023-08-23 11:11:36 +0200

    added tests for norms

---
+++ 02_work_doc/10_test/06_seleniumSystemTests/Src/PageObject/Pages/TechnicalStandartsPage.py
@@ -0,0 +1,37 @@
+"""
+
+"""
+import sys
+sys.path.append(sys.path[0] + "/....")
+
+from selenium import (
+    webdriver,
+)
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+
+from Src.PageObject.Locators import Locator
+
+class TechnicalStandartsPage(object):
+    """
+    
+    """
+
+    def __init__(self, driver):
+        """
+        
+        """
+        self.driver = driver
+
+    def getNormsLink(self):
+        """Return the Link to the Norm-Page
+        
+        """
+        return self.driver.find_element(By.XPATH, Locator.linkToNormsOnTS)
+
+    def getProtocolsLink(self):
+        """Return the Link to the Protocols-Page
+        
+        """
+        return self.driver.find_element(By.XPATH, Locator.linkToProtocolsOnTS)
+    
