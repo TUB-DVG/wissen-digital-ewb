96ccfb8d081e7531c77eb0c8461a1ef41f9aae7e
Tobias <m.degner@tu-berlin.de>
2023-10-16 14:23:55 +0200

    solved problem with new tab openings on link clicking

--- 02_work_doc/10_test/06_seleniumSystemTests/Test/TestSuite/TestRunner.py
+++ 02_work_doc/10_test/06_seleniumSystemTests/Test/TestSuite/TestRunner.py
@@ -25,15 +25,15 @@ if __name__ == "__main__":
     testLoader = TestLoader()
     # Test Suite is used since there are multiple test cases
     testSuite = TestSuite((
-        # testLoader.loadTestsFromTestCase(TestDigitalToolsPage),
-        # testLoader.loadTestsFromTestCase(TestMainPage),
-        # testLoader.loadTestsFromTestCase(TestTechnicalStandarts),
-        # testLoader.loadTestsFromTestCase(TestNormsPage),
-        # testLoader.loadTestsFromTestCase(TestProtocolsPage),
-        # testLoader.loadTestsFromTestCase(TestBusinessAppPage),
-        # testLoader.loadTestsFromTestCase(TestClickThroughSites),
+        testLoader.loadTestsFromTestCase(TestDigitalToolsPage),
+        testLoader.loadTestsFromTestCase(TestMainPage),
+        testLoader.loadTestsFromTestCase(TestTechnicalStandarts),
+        testLoader.loadTestsFromTestCase(TestNormsPage),
+        testLoader.loadTestsFromTestCase(TestProtocolsPage),
+        testLoader.loadTestsFromTestCase(TestBusinessAppPage),
+        testLoader.loadTestsFromTestCase(TestClickThroughSites),
         testLoader.loadTestsFromTestCase(TestLastprofileTab),
-        # testLoader.loadTestsFromTestCase(TestAboutPage),
+        testLoader.loadTestsFromTestCase(TestAboutPage),
         # testLoader.loadTestsFromTestCase(TestAdminPage),
         ))
  
