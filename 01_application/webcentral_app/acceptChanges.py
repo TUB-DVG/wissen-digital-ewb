a63897d6e17464c6347bc264e27dfa6dabb45b87
Falk Cudok <falk.cudok@tu-berlin.de>
2023-10-20 08:17:12 +0000

    reset unnessecary change in terms of content

--- 01_application/webcentral_app/acceptChanges.py
+++ 01_application/webcentral_app/acceptChanges.py
@@ -6,12 +6,12 @@ django.setup()
 # import DatabaseDifference
 from project_listing.DatabaseDifference import DatabaseDifference
 listOfDatabaseDifferences = []
-with open("1697533907.yaml", "r") as stream:
+with open("1690272995.yaml", "r") as stream:
     for data in yaml.load_all(stream, Loader=yaml.Loader):
         # pdb.set_trace()
         data.keepCurrentState = False
         data.keepPendingState = True
         listOfDatabaseDifferences.append(data)
-with open("1697533907_edited.yaml", "w") as stream:
+with open("1690272995_edited.yaml", "w") as stream:
     yaml.dump_all(listOfDatabaseDifferences, stream)
 
