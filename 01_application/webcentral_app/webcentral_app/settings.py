af4ced8bcddd2da85d9cfa483cffdc0426cb57b9
Martin Tobias Degner <m.degner@tu-berlin.de>
2023-12-31 10:50:32 +0100

    testdatabase is used for database tests

--- 01_application/webcentral_app/webcentral_app/settings.py
+++ 01_application/webcentral_app/webcentral_app/settings.py
@@ -102,11 +102,12 @@ DATABASES = {
         'USER': os.environ.get("POSTGRES_USER"),
         'PASSWORD': os.environ.get("POSTGRES_PASSWORD"),
         'HOST': 'database',
-        'TEST': {
-            'MIRROR': "default",
+    },
+    'TEST': {
+        'MIRROR': "default",
         },
-    },   
-}
+    }   
+
 
 
 
