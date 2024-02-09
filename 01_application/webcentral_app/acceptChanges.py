cb1b28f82c16d793e35a3a29ef18aa2a01b5b8c7
Ibukun Akin <ibukun.odeyemi@tu-berlin.de>
2024-02-09 16:01:10 +0100

    new pages created for applications

--- 01_application/webcentral_app/acceptChanges.py
+++ 01_application/webcentral_app/acceptChanges.py
@@ -1,17 +1,18 @@
 import yaml
 import pdb
-
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webcentral_app.settings')
 import django 
 django.setup()
 # import DatabaseDifference
 from project_listing.DatabaseDifference import DatabaseDifference
 listOfDatabaseDifferences = []
-with open("../../postgres/2023_12_15_EWB_Tools_Uebersicht_v2_02012024_140347.yaml", "r") as stream:
+with open("2024_02_06_EWB_Tools_Uebersicht_reclassified_v2_09022024_085514.yaml", "r") as stream:
     for data in yaml.load_all(stream, Loader=yaml.Loader):
         # pdb.set_trace()
         data.keepCurrentState = False
         data.keepPendingState = True
         listOfDatabaseDifferences.append(data)
-with open("../../postgres/2023_12_15_EWB_Tools_Uebersicht_v2_02012024_140347_modified.yaml", "w") as stream:
+with open("2024_02_06_EWB_Tools_Uebersicht_reclassified_v2_09022024_085514.yaml", "w") as stream:
     yaml.dump_all(listOfDatabaseDifferences, stream)
 
