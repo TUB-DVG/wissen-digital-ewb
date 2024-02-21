4a50f81f00a62b28e76a5358a23eb0edc1c0c329
Martin Tobias Degner <m.degner@tu-berlin.de>
2024-02-21 10:59:25 +0100

    setted default language to de, started to insert translate tags into StartPage.html

--- 01_application/webcentral_app/webcentral_app/settings.py
+++ 01_application/webcentral_app/webcentral_app/settings.py
@@ -174,7 +174,12 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGES = [
+    ('en', ('English')),
+    ('de', ('German')),
+]
+
+LANGUAGE_CODE = 'de'
 
 TIME_ZONE = 'UTC'
 
