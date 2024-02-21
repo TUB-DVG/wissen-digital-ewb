999ba3ad1f2423848d12eb5da70a089db67b8696
Martin Tobias Degner <m.degner@tu-berlin.de>
2024-02-21 12:20:59 +0100

    added the plain-text language file. Please note, that this file needs to be compiled

--- 01_application/webcentral_app/webcentral_app/settings.py
+++ 01_application/webcentral_app/webcentral_app/settings.py
@@ -204,7 +204,10 @@ CHANNEL_LAYERS={
     }
 }
 """
-
+LOCALE_PATHS = ( 
+os.path.join(BASE_DIR, "locale"),
+# os.path.join(BASE_DIR, "yourapp/locale"), 
+)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
