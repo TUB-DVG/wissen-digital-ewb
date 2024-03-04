f9f283880e54b1ee5b99b189702d6b55efcf88a5
Martin Tobias Degner <m.degner@tu-berlin.de>
2024-03-04 08:03:11 +0100

    installed django-modeltranslation, now it needs to be configured

--- 01_application/webcentral_app/criteriaCatalog/tests.py
+++ 01_application/webcentral_app/criteriaCatalog/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
-
+from django.core import management
+from criteriaCatalog.models import (
+    CriteriaCatalog,
+    Topic,
+)
 # Create your tests here.
+class AutomaticDataImport(TestCase):
+    
+    def testLoadExcelFile(self):
+        """
+        """
+        # breakpoint()
+        management.call_command('data_import', '../../02_work_doc/01_daten/08_criteriaCatalog/Tabelle_Wissensplattform_criteriaCatalog_Personalisierung.xlsx', "tests/data")
+
+        self.assertEqual(CriteriaCatalog.objects.count(), 1)
+        self.assertTrue(CriteriaCatalog.objects.filter(name="Profilbildung und Personalisierung").exists())
+
+        self.assertGreater(Topic.objects.count(), 0)
+        self.assertTrue(Topic.objects.filter(text="Spezifikation der Datenkategorien").parentId.id == 1)
+        self.assertTrue(Topic.objects.filter(text="Personenbezug der Daten").parentId.id == None)
--- 01_application/webcentral_app/criteriaCatalog/urls.py
+++ 01_application/webcentral_app/criteriaCatalog/urls.py
@@ -3,10 +3,11 @@ from django.urls import path
 from . import views
 from django.views.generic.base import TemplateView
 
-app_name = "criteriaCatalog"
+# app_name = "criteriaCatalog"
 
 urlpatterns = [
-    path('', views.index, name="index"),
-    path('<str:id>', views.useCaseView, name='usecase_view'),
+    # path('', views.index, name="index"),
+    # path('<str:id>', views.useCaseView, name='usecase_view'),
+    path("<str:criteriaCatalogIdentifer>", views.buildCrtieriaCatalog),
     # path('warmelast', views.warmelast, name='LastProfile_warme_display'),   
 ]
