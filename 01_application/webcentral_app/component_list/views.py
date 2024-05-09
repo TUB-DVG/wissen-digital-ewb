d0b475096252ef42635e37229f29ae24249bf880
Martin Tobias Degner <m.degner@tu-berlin.de>
2024-05-09 14:32:06 +0200

    wrote test to check if selection fields holds the data from Category and componentClass table

--- 01_application/webcentral_app/component_list/views.py
+++ 01_application/webcentral_app/component_list/views.py
@@ -1,8 +1,48 @@
 from django.shortcuts import render
 
+from .models import (
+    Category,
+    Component,
+    ComponentClass,
+)
+
+
 # Create your views here.
 def components(request):
-    return render(request, 'component_list/components.html')
+    context = {
+        "focusBorder":
+        "ecological",
+        "urlName":
+        "components",
+        "optionList": [
+            {
+                "placeholder":
+                "Kategorie",
+                "objects": [
+                    categoryItem.category
+                    for categoryItem in Category.objects.all()
+                ],
+                # "filter":
+                # filteredBy[0],
+                "fieldName":
+                "category",
+            },
+            {
+                "placeholder":
+                "Komponente",
+                "objects": [
+                    componentItem.componentClass
+                    for componentItem in ComponentClass.objects.all()
+                ],
+                # "filter":
+                # filteredBy[1],
+                "fieldName":
+                "component",
+            },
+        ],
+    }
+    return render(request, "component_list/components.html", context)
+
 
 def dataProcessing(request):
-    return render(request, 'component_list/dataProcessing.html')
+    return render(request, "component_list/dataProcessing.html")
