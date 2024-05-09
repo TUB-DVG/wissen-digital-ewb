6fca584c2632b0442815875a41d7d1eb0fc7f757
Martin Tobias Degner <m.degner@tu-berlin.de>
2024-05-09 11:20:40 +0200

    added data_import method for components

--- 01_application/webcentral_app/component_list/models.py
+++ 01_application/webcentral_app/component_list/models.py
@@ -1,20 +1,21 @@
 from django.db import models
 
+
 class Component(models.Model):
     category = models.CharField(max_length=255)
     component = models.CharField(max_length=255)
-    description = models.TextField()
-    energyConsumptionUsePhaseTotal = models.FloatField()
-    globalWarmingPotentialTotal = models.FloatField()
-    componentWeight = models.FloatField()
-    lifetime = models.IntegerField()
-    energyConsumptionUsePhaseActive = models.FloatField()
-    energyConsumptionUsePhasePassive = models.FloatField()
-    globalWarmingPotentialProduction = models.FloatField()
-    globalWarmingPotentialUsePhase = models.FloatField()
-    globalWarmingPotentialEndOfLife = models.FloatField()
+    description = models.TextField(blank=True, null=True)
+    energyConsumptionUsePhaseTotal = models.FloatField(blank=True, null=True)
+    globalWarmingPotentialTotal = models.FloatField(blank=True, null=True)
+    componentWeight = models.FloatField(blank=True, null=True)
+    lifetime = models.IntegerField(blank=True, null=True)
+    energyConsumptionUsePhaseActive = models.FloatField(blank=True, null=True)
+    energyConsumptionUsePhasePassive = models.FloatField(blank=True, null=True)
+    globalWarmingPotentialProduction = models.FloatField(blank=True, null=True)
+    globalWarmingPotentialUsePhase = models.FloatField(blank=True, null=True)
+    globalWarmingPotentialEndOfLife = models.FloatField(blank=True, null=True)
     furtherInformationNotes = models.TextField(blank=True)
-    sources = models.TextField()
+    sources = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.component
