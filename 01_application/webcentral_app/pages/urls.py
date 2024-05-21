6e2aebbf8aba38ba50d810f0fcee1d5d91b12e2a
Martin Tobias Degner <m.degner@tu-berlin.de>
2024-05-21 08:15:18 +0200

    implemented first version of detailsPage for dataSufficiency

--- 01_application/webcentral_app/pages/urls.py
+++ 01_application/webcentral_app/pages/urls.py
@@ -1,36 +1,62 @@
 """Include url conneced to view functions."""
 from django.urls import path
+
 # from django.shortcuts import redirect
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('impressum', views.impressum, name='impressum'),
-    path('about', views.about, name='about'),
-    path('coming', views.coming, name='coming'),
-    path('Datenschutzhinweis', views.Datenschutzhinweis,
-         name='Datenschutzhinweis'),
-    path('businessModelsChallenge', views.businessModelsChallenge,
-         name='businessModelsChallenge'),
-    path('businessModelsPractice', views.businessModelsPractice,
-         name='businessModelsPractice'),
-    path('userIntegrationPractice', views.userIntegrationPractice,
-         name='userIntegrationPractice'),
-    path('userIntegrationMethod', views.userIntegrationMethod,
-         name='userIntegrationMethod'),
-    path('environmentalIntegrityNegativ', views.environmentalIntegrityNegativ,
-         name='environmentalIntegrityNegativ'),
-    path('environmentalIntegrityPositiv', views.environmentalIntegrityPositiv,
-         name='environmentalIntegrityPositiv'),
-    path('benchmarkingChallenges', views.benchmarkingChallenges,
-         name='benchmarkingChallenges'),
-    path('dataSufficiency', views.dataSufficiency,
-         name='dataSufficiency'),
-    path('dataSecurity', views.dataSecurity,
-         name='dataSecurity'),
-    path('iconsAndVis', views.iconsAndVis,
-         name='iconsAndVis'),
-    path('criteriaCatalog', views.criteriaCatalog,
-         name='criteriaCatalog'),
-#     redirect('criteriaCatalog:index'),
+    path("", views.index, name="index"),
+    path("impressum", views.impressum, name="impressum"),
+    path("about", views.about, name="about"),
+    path("coming", views.coming, name="coming"),
+    path(
+        "Datenschutzhinweis",
+        views.Datenschutzhinweis,
+        name="Datenschutzhinweis",
+    ),
+    path(
+        "businessModelsChallenge",
+        views.businessModelsChallenge,
+        name="businessModelsChallenge",
+    ),
+    path(
+        "businessModelsPractice",
+        views.businessModelsPractice,
+        name="businessModelsPractice",
+    ),
+    path(
+        "userIntegrationPractice",
+        views.userIntegrationPractice,
+        name="userIntegrationPractice",
+    ),
+    path(
+        "userIntegrationMethod",
+        views.userIntegrationMethod,
+        name="userIntegrationMethod",
+    ),
+    path(
+        "environmentalIntegrityNegativ",
+        views.environmentalIntegrityNegativ,
+        name="environmentalIntegrityNegativ",
+    ),
+    path(
+        "environmentalIntegrityPositiv",
+        views.environmentalIntegrityPositiv,
+        name="environmentalIntegrityPositiv",
+    ),
+    path(
+        "benchmarkingChallenges",
+        views.benchmarkingChallenges,
+        name="benchmarkingChallenges",
+    ),
+    path("dataSufficiency", views.dataSufficiency, name="dataSufficiency"),
+    path(
+        "dataSufficiencyBox/<int:idOfPage>/",
+        views.dataSufficiencyBox,
+        name="dataSufficiencyBox",
+    ),
+    path("dataSecurity", views.dataSecurity, name="dataSecurity"),
+    path("iconsAndVis", views.iconsAndVis, name="iconsAndVis"),
+    path("criteriaCatalog", views.criteriaCatalog, name="criteriaCatalog"),
+    #     redirect('criteriaCatalog:index'),
 ]
