488f0542c5fd619ec0fbe4fea232cf84a6a4e3e1
Martin Tobias Degner <m.degner@tu-berlin.de>
2024-06-18 17:21:48 +0200

    fixed apearance of select elements in search-bar through out all listing pages

--- 01_application/webcentral_app/weatherdata_over/views.py
+++ 01_application/webcentral_app/weatherdata_over/views.py
@@ -1,32 +1,34 @@
 from django.shortcuts import (
-    render, 
+    render,
     get_object_or_404,
 )
 from django.core.paginator import Paginator
 from django.utils.translation import gettext as _
 
-from .models import Weatherdata 
+from .models import Weatherdata
+
 
 # Create your views here.
 class UpdateProperties:
+
     def __init__(self, class_name, label, color_class):
         self.class_name = class_name
         self.label = label
         self.color_class = color_class
 
+
 def index(request):
     """
     shows the list of all projects including some key features
     """
-    weatherdata = Weatherdata.objects.all() # reads all data from table Teilprojekt       
-    filtered_by = [None]*2
+    weatherdata = (Weatherdata.objects.all()
+                   )  # reads all data from table Teilprojekt
+    filtered_by = [None] * 2
     searched = None
 
-    if (
-        (_checkDjangoDict(request, _("Kategorie")) is not None) | 
-        (_checkDjangoDict(request, _("Lizenz")) is not None) |
-        (_checkDjangoDict(request, "searched") is not None)
-    ):
+    if ((_checkDjangoDict(request, _("Kategorie")) is not None)
+            | (_checkDjangoDict(request, _("Lizenz")) is not None)
+            | (_checkDjangoDict(request, "searched") is not None)):
         category = request.GET.get(_("Kategorie"), "")
         license = request.GET.get(_("Lizenz"), "")
         searched = request.GET.get("searched", "")
@@ -36,87 +38,104 @@ def index(request):
             data_service__icontains=searched,
         )
         filtered_by = [category, license]
-    
-    weatherdata = list(sorted(weatherdata, key=lambda obj:obj.data_service))
 
-    weatherdata_paginator = Paginator(weatherdata,12)
+    weatherdata = list(sorted(weatherdata, key=lambda obj: obj.data_service))
 
-    pageNum = request.GET.get('page', None)
+    weatherdata_paginator = Paginator(weatherdata, 12)
+
+    pageNum = request.GET.get("page", None)
     page = weatherdata_paginator.get_page(pageNum)
 
     context = {
-        'page': page,
-        'search': searched,
-        'kategorie': filtered_by[0],
-        'lizenz': filtered_by[1],
-        "nameOfTemplate": "weatherdata",
-        "urlName": "publicationPage",
+        "page":
+        page,
+        "search":
+        searched,
+        "kategorie":
+        filtered_by[0],
+        "lizenz":
+        filtered_by[1],
+        "nameOfTemplate":
+        "weatherdata",
+        "urlName":
+        "publicationPage",
         "optionList": [
             {
-                "placeholder": _("Kategorie"), 
+                "placeholder": _("Kategorie"),
                 "objects": [
-                    _("Datensätze"), 
+                    _("Datensätze"),
                     _("Anwendung"),
                 ],
+                "fieldName": "category",
                 "filter": filtered_by[0],
             },
             {
-                "placeholder": _("Lizenz"), 
+                "placeholder":
+                _("Lizenz"),
                 "objects": [
-                    _("Frei nutzbar"), 
-                    "Open Data", 
-                    "CC BY 4.0", 
-                    _("MIT-Lizenz"), 
+                    _("Frei nutzbar"),
+                    "Open Data",
+                    "CC BY 4.0",
+                    _("MIT-Lizenz"),
                 ],
-                "filter": filtered_by[1],
+                "fieldName":
+                "license",
+                "filter":
+                filtered_by[1],
             },
         ],
-        "focusBorder": "technical",
+        "focusBorder":
+        "technical",
     }
-    return render(request, 'weatherdata_over/data-service-listings.html', context)
+    return render(request, "weatherdata_over/data-service-listings.html",
+                  context)
+
 
 def weatherdata_view(request, id):
-    
     """
     shows of the key features one project
     """
 
     category_icons = {
-#        'window': 'bi bi-window',
-        'Anwendung': 'bi bi-terminal fa-lg',
-        'Datensätze': 'fas fa-database',
-        'default': 'fas fa-bars',
+        #        'window': 'bi bi-window',
+        "Anwendung": "bi bi-terminal fa-lg",
+        "Datensätze": "fas fa-database",
+        "default": "fas fa-bars",
         "focusBorder": "technical",
     }
 
-    weatherdata = get_object_or_404(Weatherdata, pk= id)
-    
-    letztes_update = UpdateProperties('bi bi-patch-exclamation-fill', 'letztes Update', 'text-danger')
-    laufende_updates = UpdateProperties('fas fa-sync', 'Updates', 'text-success')
+    weatherdata = get_object_or_404(Weatherdata, pk=id)
 
-    #changing labels and icon
+    letztes_update = UpdateProperties("bi bi-patch-exclamation-fill",
+                                      "letztes Update", "text-danger")
+    laufende_updates = UpdateProperties("fas fa-sync", "Updates",
+                                        "text-success")
+
+    # changing labels and icon
     update_properties = letztes_update
-    if (weatherdata.last_update == 'laufend'):
+    if weatherdata.last_update == "laufend":
         update_properties = laufende_updates
 
-    category_icon = category_icons['default']
-    if (weatherdata.category in category_icons):
+    category_icon = category_icons["default"]
+    if weatherdata.category in category_icons:
         category_icon = category_icons[weatherdata.category]
 
     context = {
-        'weatherdata': weatherdata,
-        'letztes_update': update_properties,
-        'letztes_update_class': update_properties.class_name,
-        'letztes_update_color': update_properties.color_class,
-        'letztes_update_label': update_properties.label,
-        'category_icon': category_icon,
+        "weatherdata": weatherdata,
+        "letztes_update": update_properties,
+        "letztes_update_class": update_properties.class_name,
+        "letztes_update_color": update_properties.color_class,
+        "letztes_update_label": update_properties.label,
+        "category_icon": category_icon,
         "focusBorder": "technical",
     }
 
-    return render(request, 'weatherdata_over/weatherdata-detail.html', context)
+    return render(request, "weatherdata_over/weatherdata-detail.html", context)
+
 
 def wetterdienst(request):
-    return render(request,'weatherdata_over/wetterdienst(example).html')
+    return render(request, "weatherdata_over/wetterdienst(example).html")
+
 
 def _checkDjangoDict(request, key):
     if key in request.GET:
