ce18b800a1c0530770c26abcac0199b058063a75
Martin Tobias Degner <m.degner@tu-berlin.de>
2024-06-20 13:18:45 +0200

    prepared components-model for import of english translations, added up_initial command to run script

--- 01_application/webcentral_app/component_list/admin.py
+++ 01_application/webcentral_app/component_list/admin.py
@@ -1,4 +1,6 @@
+from modeltranslation.admin import TranslationAdmin
 from django.contrib import admin
+
 from .models import (
     Component,
     ComponentClass,
@@ -7,8 +9,21 @@ from .models import (
     DataSufficiency,
 )
 
-admin.site.register(Component)
-admin.site.register(ComponentClass)
-admin.site.register(Category)
+
+class ComponentModelAdmin(TranslationAdmin):
+    pass
+
+
+class ComponentClassModelAdmin(TranslationAdmin):
+    pass
+
+
+class CategoryModelAdmin(TranslationAdmin):
+    pass
+
+
+admin.site.register(Component, ComponentModelAdmin)
+admin.site.register(ComponentClass, ComponentClassModelAdmin)
+admin.site.register(Category, CategoryModelAdmin)
 admin.site.register(EnvironmentalImpact)
 admin.site.register(DataSufficiency)
