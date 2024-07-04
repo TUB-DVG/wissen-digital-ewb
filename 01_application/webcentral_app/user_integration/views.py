3fbe1be84bf95387d27f75c12dbfa39ed8d6bdaf
Martin Tobias Degner <m.degner@tu-berlin.de>
2024-07-04 08:00:00 +0200

    removed collapse container from explanation text on user integration overview site, removed list on the right site in boxes to detaisl site, aligned arrow with backLinktext on backLinkInFocusColor

--- 01_application/webcentral_app/user_integration/views.py
+++ 01_application/webcentral_app/user_integration/views.py
@@ -10,38 +10,28 @@ def userEngagementDetails(request, engagementId):
     """Serve the user engagement details page."""
     userEngagementObj = UserEngagement.objects.get(id=engagementId)
     context = {
-        "imageInBackButton":
-        "assets/images/backArrowOperational.svg",
-        "focusBorder":
-        "operational",
-        "boxObject":
-        userEngagementObj,
-        "backLinkText":
-        _("Nutzendenintegration"),
-        "backLink":
-        "userEngagement",
-        "leftColumn":
-        "user_integration/userEngagementDetailsLeftColumn.html",
+        "imageInBackButton": "assets/images/backArrowOperational.svg",
+        "focusBorder": "operational",
+        "boxObject": userEngagementObj,
+        "backLinkText": _("Übersicht Methoden Nutzendenintegration"),
+        "backLink": "userEngagement",
+        "leftColumn": "user_integration/userEngagementDetailsLeftColumn.html",
         "rightColumn":
         "user_integration/userEngagementDetailsRightColumn.html",
-        "imageQuickLinks":
-        True,
-        "idOfSelectedObj":
-        engagementId,
-        "allObjectsForQuickLinks":
-        UserEngagement.objects.all(),
-        "showInputsInImageQuickLinkBar":
-        False,
-        "showSelect":
-        True,
-        "tags": [
-            currentUserEnagementObj.category
-            for currentUserEnagementObj in UserEngagement.objects.all()
-        ],
-        "quickLinkName":
-        "user_integration/userEngagement",
-        "javascriptFilePath":
-        "js/user_integration_functions.js",
+        "imageQuickLinks": False,
+        "idOfSelectedObj": engagementId,
+        # "allObjectsForQuickLinks":
+        # UserEngagement.objects.all(),
+        # "showInputsInImageQuickLinkBar":
+        # False,
+        # "showSelect":
+        # False,
+        # "tags": [
+        #     currentUserEnagementObj.category
+        #     for currentUserEnagementObj in UserEngagement.objects.all()
+        # ],
+        "quickLinkName": "user_integration/userEngagement",
+        "javascriptFilePath": "js/user_integration_functions.js",
     }
 
     return render(request, "pages/detailsPage.html", context)
