5002d128c106651e84a09030d75151b86a3c9a1e
Martin Tobias Degner <m.degner@tu-berlin.de>
2024-07-10 12:54:31 +0200

    did a migration with --fake-initial to solve the conflicts in the migration system

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: './01_application/webcentral_app/webcentral_app/static/js/main.js',
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css'
+        }),
+        new HtmlWebpackPlugin({
+            template: './01_application/webcentral_app/templates/base.html'
+        })
+    ],
+    mode: 'production'
+};
