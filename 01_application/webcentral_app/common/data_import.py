001589e5a4348b7410613789896cac0ac0947363
Martin Tobias Degner <m.degner@tu-berlin.de>
2024-07-12 07:55:57 +0200

    moved creation of test-excel file into seperate python file

--- 01_application/webcentral_app/common/data_import.py
+++ 01_application/webcentral_app/common/data_import.py
@@ -6,4 +6,40 @@ class DataImport:
         """
         self.path_to_file = path_to_data_file
 
-    def to 
+    def load(self):
+        """load csv/excel-file
+
+        """
+        
+        # if its a excel file, check if 2 sheets are present:
+
+
+        if self.path_to_file.endswith(".csv"):
+            header, data = self.readCSV(pathFile)
+        elif self.path_to_file.endswith(".xlsx"):
+            header, data = self.readExcel(pathFile)
+        else:
+            raise CommandError(
+                "Invalid file format. Please provide a .csv or .xlsx file.")
+
+    def readExcel(
+            self,
+            path: str,
+        ) -> tuple:
+        """This method reads the excel-file, and loads the content into
+        the two variables header and data.
+
+        Parameters:
+        path:   str
+
+        Returns:
+        header: list
+            List of headers from the excel-file.
+        data:   list
+            list, containing the rows from the excel-file.
+        """
+        df = pd.read_excel(path)
+        df = df.fillna("")
+        header = list(df.columns)
+        data = df.values.tolist()
+        return header, data
