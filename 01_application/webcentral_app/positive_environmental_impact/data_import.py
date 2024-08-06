eb57220c2279ab3e75af36e30b5827685dc1818f
Martin Tobias Degner <m.degner@tu-berlin.de>
2024-08-06 14:22:33 +0200

    tested dataimport of positive_environmental_impact

--- 01_application/webcentral_app/positive_environmental_impact/data_import.py
+++ 01_application/webcentral_app/positive_environmental_impact/data_import.py
@@ -13,34 +13,17 @@ class DataImportApp(DataImport):
     
     MAPPING_EXCEL_DB_EN = {
         "Category__en": "category_en",
-        # "resources_en": "resources_en",
-        # "applicationArea_en": "applicationArea_en",
-        # "provider_en": "provider_en",
-        # "usage_en": "usage_en",
-        # "lifeCyclePhase_en": "lifeCyclePhase_en",
-        # "targetGroup_en": "targetGroup_en",
-        # "userInterface_en": "userInterface_en",
-        # "userInterfaceNotes_en": "userInterfaceNotes_en",
-        # "programmingLanguages_en": "programmingLanguages_en",
-        # "frameworksLibraries_en": "frameworksLibraries_en",
-        # "databaseSystem_en": "databaseSystem_en",
-        # "classification_en": "classification_en",
-        # "focus_en": "focus_en",
-        # "scale_en": "scale_en",
-        # "lastUpdate_en": "lastUpdate_en",
-        # "accessibility_en": "accessibility_en",
-        # "license_en": "license_en",
-        # "licenseNotes_en": "licenseNotes_en",
-        # "furtherInformation_en": "furtherInformation_en",
-        # "alternatives_en": "alternatives_en",
-        # "specificApplication_en": "specificApplication_en",
-        # "released_en": "released_en",
-        # "releasedPlanned_en": "releasedPlanned_en",
-        # "yearOfRelease_en": "yearOfRelease_en",
-        # "developmentState_en": "developmentState_en",
-        # "technicalStandardsNorms_en": "technicalStandardsNorms_en",
-        # "technicalStandardsProtocols_en": "technicalStandardsProtocols_en",
-        # "image_en": "image_en",
+        "Description__en": "description_en",
+        "Name_Digital_Application__en": "name_digital_application_en",
+        "Project_Name__en": "project_name_en",
+        "Partner__en": "partner_en",
+        "Consortium__en": "consortium_en",
+        # "Additional_Digital_Application(s)__en": "digitalApplications_en",
+        "Strategies__en": "strategies_en",
+        "Relevance__en": "relevance_en",
+        "Problem_Statement_and_Problem_Goals__en": "problem_statement_and_problem_goals_en",
+        "Implementation_in_the_Project__en": "implementation_in_the_project_en",
+        "Evaluation__en": "evaluation_en"
     }
     def __init__(self, path_to_data_file):
         """Constructor of the app-specific data_import
@@ -76,74 +59,83 @@ class DataImportApp(DataImport):
         created:    bool
             Indicates, if the UserIntegration-object was created or not.
         """
-        
-        
-
         category = row[header.index("Category")]
-        category = row[header.index("Category")]
+        description = row[header.index("Description")]
+        nameDigitalApplication = row[header.index("Name_Digital_Application")]
+        projectname = row[header.index("Projektname")]
+        fundingLabel = row[header.index("Funding_Label")]
+        duration = row[header.index("Duration")]
+        partner = row[header.index("Partner")]
+        projectWebsite = row[header.index("Project_Website")]
+        consortium = row[header.index("Consortium")]
+        further = row[header.index("Further")]
+        digitalApplications = row[header.index("Digital_applications")]
+        goals = row[header.index("Goals")]
+        strategies = row[header.index("Strategies")]
+        relevance = row[header.index("Relevance")]
+        image = row[header.index("Image")]
+        problemStatementAndProblemGoals = row[header.index("Problem_Statement_and_Problem_Goals")]
+        implementationInTheProject = row[header.index("Implementation_in_the_Project")]
+        evaluation = row[header.index("Evaluation")]
+        projectName = row[header.index("Project_Name")]
+        weiterführendeLiteratur = row[header.index("Weiterführende Literatur")]
 
-        groupSize = row[header.index("Gruppengröße")]
-        material = row[header.index("Material")]
-        goals = row[header.index("Ziele")]
-        goodPracticeExample = row[header.index("Good-Practice-Beispiel")]
-        obj, created = UserEngagement.objects.get_or_create(
-            category=category,
-            categoryShortDescription=categoryShortDescription,
-            subCategory=subCategory,
-            subCategoryShortDescription=subCategoryShortDescription,
-            subtitle=subtitle,
-            timeRequired=timeRequired,
-            groupSize=groupSize,
-            material=material,
-            goals=goals,
-            goodPracticeExample=goodPracticeExample,
+
+
+        obj, created = EnvironmentalImpact.objects.get_or_create(
+            category_de=category,
+            description_de=description,
+            name_digital_application_de=nameDigitalApplication,
+            project_name_de=projectname,
+            partner_de=partner,
+            project_website=projectWebsite,
+            consortium_de=consortium,
+            further_de=further,
+            digitalApplications_de=digitalApplications,
+            goals_de=goals,
+            strategies_de=strategies,
+            relevance_de=relevance,
+            image=image,
+            problem_statement_and_problem_goals_de=problemStatementAndProblemGoals,
+            implementation_in_the_project_de=implementationInTheProject,
+            evaluation_de=evaluation,
         )
-        procedureList = self._processListInput(row[header.index("Ablauf")],
+        fundingLabelList = self._processListInput(row[header.index("Funding_Label")],
                                                ";;")
-        procedureObjList = [
-            ProcedureItem.objects.get_or_create(_procedureItem=procedure)[0]
-            for procedure in procedureList
+        fundingLabelObjList = [
+            Subproject.objects.get_or_create(referenceNumber_id=fkzItem)[0]
+            for fkzItem in fundingLabelList
         ]
-        conArgumentsList = self._processListInput(
-            row[header.index("Nachteile")], ";;")
-        conObjsList = [
-            ConArgument.objects.get_or_create(conArgument=conArgElement)[0]
-            for conArgElement in conArgumentsList
-        ]
-        proArgumentsList = self._processListInput(
-            row[header.index("Vorteile")], ";;")
-        proObjsList = [
-            ProArgument.objects.get_or_create(proArgument=proArgElement)[0]
-            for proArgElement in proArgumentsList
-        ]
-        literatureList = self._processListInput(row[header.index("Literatur")],
-                                                ";;")
-        literatureObjsList = []
-        for literatureElement in literatureList:
-            splittedLiteratureElement = literatureElement.split("((")
-            literatureString = splittedLiteratureElement[0]
-            literatureIdentifer = splittedLiteratureElement[1].replace(
-                "))", "")
-            literatureObj, _ = Literature.objects.get_or_create(
-                literature=literatureString,
-                linkName=literatureIdentifer,
-            )
-            literatureObjsList.append(literatureObj)
-        obj.procedure.add(*procedureObjList)
-        obj.proArguments.add(*proObjsList)
-        obj.conArguments.add(*conObjsList)
-        obj.literature.add(*literatureObjsList)
+        # literatureList = self._processListInput(row[header.index("Literatur")],
+        #                                         ";;")
+        # literatureObjsList = []
+        # for literatureElement in literatureList:
+        #     splittedLiteratureElement = literatureElement.split("((")
+        #     literatureString = splittedLiteratureElement[0]
+        #     literatureIdentifer = splittedLiteratureElement[1].replace(
+        #         "))", "")
+        #     literatureObj, _ = Literature.objects.get_or_create(
+        #         literature=literatureString,
+        #         linkName=literatureIdentifer,
+        #     )
+        #     literatureObjsList.append(literatureObj)
+        obj.funding_label.add(*fundingLabelObjList)
+        # obj.literature.add(*literatureObjsList)
         
         if self._englishHeadersPresent(header):
-            _getOrCreateEnglishTranslation(row, header, data, obj)
+           self._getOrCreateEnglishTranslation(row, header, data, obj)
     
     def _getOrCreateEnglishTranslation(self, row: list, header: list, data: list, environmentalimpactObj):
         """
 
         """
         for mappingKey in self.MAPPING_EXCEL_DB_EN.keys():
-            setattr(obj, self.MAPPING_EXCEL_DB_EN[mappingKey], data[header.index(mappingKey)])
-
+            # attributeNameWithoutEn = self.MAPPING_EXCEL_DB_EN[mappingKey].remove("__en") 
+            # if hasattr(obj, attributeNameWithoutEn) 
+            try:
+                setattr(environmentalimpactObj, self.MAPPING_EXCEL_DB_EN[mappingKey], row[header.index(mappingKey)])
+            except:
+                breakpoint()
 
 
     def _englishHeadersPresent(self, header: list) -> bool:
