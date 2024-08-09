c913834c9f0a79e69d92ca2fc9132b87e5230db9
Martin Tobias Degner <m.degner@tu-berlin.de>
2024-08-09 10:01:00 +0200

    added function on details-page to show dots when collapse element is collapsed

--- 02_work_doc/10_test/06_seleniumSystemTests/Test/Scripts/TestDataSufficiency.py
+++ 02_work_doc/10_test/06_seleniumSystemTests/Test/Scripts/TestDataSufficiency.py
@@ -19,7 +19,7 @@ from Src.PageObject.Pages.NegativeEnvironmentalImpacts import (
 from Src.PageObject.Pages.DataSufficiency import DataSufficiency
 
 
-class TestComponentsList(WebDriverSetup):
+class TestDataSufficiency(WebDriverSetup):
     """Represent the Selenium-Test of the Components-List Page"""
 
     def testPageStructure(self):
@@ -41,3 +41,24 @@ class TestComponentsList(WebDriverSetup):
         divFourCardsContainer = dataSufficiencyObj.getDescendantsByClass(
             contentDiv, "fourCardsContainer")
         self.assertEqual(len(divFourCardsContainer), 1)
+            
+    def testDotsPresentWhenCollapsed(self):
+        """Test if three dots are present at the end of the collapsed component. 
+        The three dots indicate, that there is more text to be shown when clicking
+        on `show more`
+        """
+        self.driver.get(os.environ["siteUnderTest"] + "/pages/dataSufficiency")
+        dataSufficiencyObj = DataSufficiency(driver)
+
+        allFirstPartsOfCollapsables = dataSufficiencyObj.getAllCollapsableFirstParts()
+        for firstPartOfCollapsable in allFirstPartsOfCollapsables:
+            parentDiv = dataSufficiencyObj.getFirstAncestorByTagName(firstPartOfCollapsable, "div")
+            if "Show more" in parentDiv.text or "Zeige mehr" in parentDiv.text:
+                nextSibling = dataSufficiencyObj.getFollowingSiblingOfTagName(firstPartOfCollapsable, "div")
+                if "collapse" in nexSibling.get_attribute("class"):
+                    self.assertEqual(firstPartOfCollapsable.text[-3:], "...", "The last 3 characters should be dots, when the element is collapsed.")
+                elif "show" in nextSibling.get_attribute("class"):
+                    self.assertNotEqual(firstPartOfCollapsable.text[-3:], "...", "The last 3 characters should be dots, when the element is collapsed.")
+
+
+
