912ae7545c1a94f54701898f2e990012a21ef960
Martin Tobias Degner <m.degner@tu-berlin.de>
2024-08-16 12:54:54 +0200

    edited app component_list app name to snake case

--- 01_application/webcentral_app/criteria_catalog/migrations/0001_initial.py
+++ 01_application/webcentral_app/criteria_catalog/migrations/0001_initial.py
@@ -13,7 +13,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='CriteriaCatalog',
+            name='Criteria_catalog',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=100)),
--- 01_application/webcentral_app/criteria_catalog/migrations/0004_alter_criteriacatalog_text.py
+++ 01_application/webcentral_app/criteria_catalog/migrations/0004_alter_criteriacatalog_text.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('criteriaCatalog', '0003_topic_grey'),
+        ('criteria_catalog', '0003_topic_grey'),
     ]
 
     operations = [
