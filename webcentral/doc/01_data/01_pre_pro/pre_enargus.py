ca3092946173686380f40a26a982f1c467e2f31a
Martin Tobias Degner <m.degner@tu-berlin.de>
2024-08-22 12:09:46 +0200

    renamed 01_daten to 01_data and mved old enargus data files

---
+++ webcentral/doc/01_data/01_pre_pro/pre_enargus.py
@@ -0,0 +1,39 @@
+'''
+    File name: pre_enargus.py
+    Author: Falk Cudok
+    Date created: 2022-02-16
+    Date last modified:
+    Python Version: 3.9
+    Description: This file is a script for preprocessing the data
+                 from enargus xml file to an csv file for later
+                 load into the data base of webcentral.
+   Virtual Env: data preprocessing, see requirement_pre.txt
+   Use own lib: dvg_lib/ProjektListe
+'''
+import os
+import sys
+
+relativePathToScript, filenameofScript = os.path.split(sys.argv[0])
+absolutePathToScript = os.getcwd() + "/" + relativePathToScript
+parentDirectory = os.path.dirname(absolutePathToScript)
+sys.path.insert(0, parentDirectory)
+
+from evaluation import EvaluationUtils
+
+listCol = absolutePathToScript + "/" + '02_parameter_files/col_xml2csv.csv'
+xml2csv = absolutePathToScript + "/" + '02_parameter_files/col_dict_xml2csv.csv'
+
+if len(sys.argv) != 2:
+    print("Preprocessing script for enargus data: Usage: python pre_enargus.py <source xml-file> <target csv-file>")
+    print("Hint: Use the run-script to execute this script.")
+    exit
+
+pathXML = "/" + sys.argv[1]
+targetCsvFile = "/" + sys.argv[2]
+dataframe = EvaluationUtils.readXMLEnargus(pathXML, xml2csv, listCol)
+
+EvaluationUtils.writeDataframe2CSV(
+    dataframe, 
+    targetCsvFile, 
+    new=True,
+)
