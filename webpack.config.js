f30ba4656603b3e9f4f6fa8a606ae87475088772
Martin Tobias Degner <m.degner@tu-berlin.de>
2024-08-29 14:01:50 +0200

    fixed user integration

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,9 @@ const BundleTracker = require('webpack-bundle-tracker');
 
 module.exports = {
   context: __dirname,
-  entry: './01_application/webcentral_app/webcentral_app/static/css/webcentral.scss',
+  entry: './webcentral/src/webcentral_app/static/css/webcentral.scss',
   output: {
-    path: path.resolve(__dirname, '01_application/webcentral_app/webcentral_app/static/webpack_bundles/'),
+    path: path.resolve(__dirname, 'webcentral/src/webcentral_app/static/webpack_bundles/'),
     filename: '[name].js', // Not used for CSS but required by Webpack
   },
   module: {
@@ -25,6 +25,6 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: 'webcentral.[contenthash].css',
     }),
-    new BundleTracker({ path: `${__dirname}/01_application/webcentral_app/`, filename: 'webpack-stats.json' })
+    new BundleTracker({ path: `${__dirname}/webcentral/src/webcentral_app/`, filename: 'webpack-stats.json' })
   ],
 };
