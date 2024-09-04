f111235e9f97d4e46a0602b51d88fd92f6881dd5
Martin Tobias Degner <m.degner@tu-berlin.de>
2024-09-04 15:12:27 +0200

    renamed static folders without _

--- webcentral/doc/06_sphinx/source/conf.py
+++ webcentral/doc/06_sphinx/source/conf.py
@@ -35,11 +35,11 @@ extensions = [
     "sphinx.ext.napoleon",
 ]
 
-templates_path = ["_templates"]
+templates_path = ["templates"]
 exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "alabaster"
-html_static_path = ["_static"]
+html_static_path = ["static"]
