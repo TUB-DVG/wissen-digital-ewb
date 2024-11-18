50f6f91bb70cc8fdb57528738270c46cf30b21df
Martin Tobias Degner <m.degner@tu-berlin.de>
2024-11-18 14:27:46 +0100

    fixed missing comma in list of export elements

--- webcentral/src/protocols/data_export.py
+++ webcentral/src/protocols/data_export.py
@@ -48,7 +48,7 @@ class DataExport:
         "osiLayers": "osiLayers",
         "buildingAutomationLayer": "buildingAutomationLayer",
         "exampleProject": "exampleProject",
-        "image": "image"
+        "image": "image",
         "programmingLanguages": "programmingLanguages",         
     }
 
