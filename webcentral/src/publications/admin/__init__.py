c9b2a151ea0dff364fb46434ba727df9532a594b
Martin Tobias Degner <m.degner@tu-berlin.de>
2024-12-11 13:33:28 +0100

    added custom History template to publications admin

--- webcentral/src/publications/admin/__init__.py
+++ webcentral/src/publications/admin/__init__.py
@@ -3,12 +3,22 @@ __author__ = "Lucas Theis <lucas@theis.io>"
 __docformat__ = "epytext"
 
 from django.contrib import admin
-from publications.models import Type, List, Publication
+
+from common.admin import HistoryAdmin
+from publications.models import Type, List, Publication, History
 from .publicationadmin import PublicationAdmin
 from .typeadmin import TypeAdmin
 from .listadmin import ListAdmin
 from .orderedmodeladmin import OrderedModelAdmin
 
+class HistoryAdminApp(HistoryAdmin):
+    """ """
+
+    modelInstance = Publication
+    historyModelInstance = History
+
+
+admin.site.register(History, HistoryAdminApp)
 admin.site.register(Type, TypeAdmin)
 admin.site.register(List, ListAdmin)
 admin.site.register(Publication, PublicationAdmin)
